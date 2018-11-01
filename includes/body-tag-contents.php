4b94c980f97b1bf033339a2dbc38a10930d139f8
Jesús David Chicano Galindo <JesusD@MacBook-Pro-de-Jesus.local>
2018-11-01 09:33:43 +0100

    Edición código PHP

--- includes/body-tag-contents.php
+++ includes/body-tag-contents.php
@@ -1,19 +1,55 @@
 <!-- Bootstrap core JavaScript -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<?php if ($CURRENT_PAGE == "Index") { ?>
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<?php }
+	else { ?>
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<?php
+	}
+?>
+
 
 <!-- Plugin JavaScript -->
-<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+<?php if ($CURRENT_PAGE == "Index") { ?>
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+<?php }
+	else { ?>
+    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+<?php
+	}
+?>
+
 
 <!-- Custom scripts for this template -->
-<script src="js/grayscale.min.js"></script>
+<?php if ($CURRENT_PAGE == "Index") { ?>
+    <script src="js/grayscale.min.js"></script>
+<?php }
+	else { ?>
+    <script src="../js/grayscale.min.js"></script>
+<?php
+	}
+?>
+
 
 <!-- Más scripts de Boilerplate -->
-<script src="js/vendor/modernizr-3.6.0.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
-<script src="js/plugins.js"></script>
-<script src="js/main.js"></script>
+<?php if ($CURRENT_PAGE == "Index") { ?>
+    <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+<?php }
+	else { ?>
+    <script src="../js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+    <script src="../js/plugins.js"></script>
+    <script src="../js/main.js"></script>
+<?php
+	}
+?>
 
 <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
 <script>

f3173c31916ea1fa333292eec48127e40bb5e63c
Jesús David Chicano Galindo <JesusD@MacBook-Pro-de-Jesus.local>
2018-11-03 12:04:27 +0100

    Añadidos elementos para los análisis

--- includes/navigation.php
+++ includes/navigation.php
@@ -32,9 +32,6 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav ml-auto">
-				<li class="nav-item">
-					<a class="nav-link js-scroll-trigger" href="plantilla.php">Plantilla</a>
-				</li>
         <li class="nav-item">
           <a class="nav-link js-scroll-trigger" href="">Noticias</a>
         </li>

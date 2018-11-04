ea30ac25f189cd51ce29b6725e115680bdc83e08
davidar13 <pese1313@gmail.com>
2018-11-04 15:57:45 +0100

    accesorios

---
+++ accesorios/index.php
@@ -0,0 +1,82 @@
+<?php include("../includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("../includes/head-tag-contents.php");?>
+</head>
+<body>
+
+<?php include("../includes/navigation.php");?>
+
+<!-- CONTENIDO -->
+
+<body class="bg-light"><!--Modificado-->
+  <div class="container bg-light">
+      <!-- AUDI -->
+      <section id="projects" class="projects-section bg-light">
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="../img/analisis/audi_a5_sportback.jpg" alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white">Audi A5 Sportback, a fondo</h4>
+                  <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- BMW -->
+      <section id="projects" class="projects-section bg-light" style="margin-top:-300px">
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="../img/analisis/bmw_m2_competition.jpg" alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white">Análisis del BMW M2 Competition</h4>
+                  <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- BMW -->
+      <section id="projects" class="projects-section bg-light" style="margin-top:-300px">
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="../img/analisis/merche.jpg" alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white">Review del Mercedes AMG C 43 4MATIC</h4>
+                  <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+  </div>
+</body>
+
+<?php include("../includes/footer.php");?>
+
+<?php include("../includes/body-tag-contents.php");?>
+
+</body>
+
+</body>
+</html>

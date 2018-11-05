1a50f8b0057f031ad0fa6eb81f01e606d8d25456
Jesús David Chicano Galindo <JesusD@MacBook-Pro-de-Jesus.local>
2018-11-05 17:26:25 +0100

    Añadido enlace a un analisis

--- analisis/index.php
+++ analisis/index.php
@@ -22,8 +22,10 @@
             <div class="bg-black text-center h-100 project">
               <div class="d-flex h-100">
                 <div class="project-text w-100 my-auto text-center text-lg-left">
-                  <h4 class="text-white">Audi A5 Sportback, a fondo</h4>
-                  <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                  <a href="audi/analisis-a5-sportback.php?titulo=Audi A5 Sportback, a fondo">
+                    <h4 class="text-white">Audi A5 Sportback, a fondo</h4>
+                    <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                  </a>
                   <hr class="d-none d-lg-block mb-0 ml-0">
                 </div>
               </div>

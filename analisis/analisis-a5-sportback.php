cee75c81a8b55b998449d5f4a199fe2cc4e952ad
Jesús David Chicano Galindo <JesusD@pc179-10.14004580.14.andared.ced.junta-andalucia.es>
2018-11-09 10:15:49 +0100

    Añadido texto del analisis

--- analisis/analisis-a5-sportback.php
+++ analisis/analisis-a5-sportback.php
@@ -28,14 +28,38 @@
   <div class="container bg-light">
 
   <section id="scroll" class="projects-section bg-light">
-    <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
       <div class="col-lg-8">
-        <h1>Diseño y calidad</h1>
-        <br>
-        Lorem ipsum dolor sit amet consectetur, adipiscing elit cum viverra vivamus diam, sem tempor luctus primis. Morbi himenaeos vulputate orci pretium nostra in turpis convallis, interdum condimentum lacinia rhoncus a volutpat ullamcorper, taciti parturient placerat elementum hac quis curae. Cum torquent ultrices enim magnis aptent senectus gravida cubilia sed, sodales placerat platea class quam mus viverra pulvinar etiam, euismod donec porta nibh ac lacus leo nec.
+          <div class="contenedor-izda caja-texto">
+          <h2>Diseño y calidad</h2>
+          <p>
+            Cuando los fabricantes apuestan por el diseño en un coche, en numerosas ocasiones, el confort de marcha queda mermado.
+            Una línea de techo coupé, unas suspensiones incómodas… hacen que la experiencia de conducir un coche con carácter deportivo
+            no sea particularmente favorable para el día a día.
+          </p>
+          <p>
+            Sin embargo, Audi ha demostrado que unir estas cualidades –combinar un diseño atractivo con un interior versátil y
+            cómodo– es completamente compatible. Y lo ha hecho con el Audi A5 Sportback, una variante coupé del A4 que se caracteriza
+            por sus cinco puertas –portón incluido– y porque es uno de los coupés más vendidos en España. Una vez que estamos dentro
+            del modelo, lo primero que nos llama la atención es que es razonablemente amplio, principalmente en las plazas delanteras.
+          </p>
+          </div>
       </div>
       <div class="col-lg-4">
-      Lorem ipsum dolor sit amet consectetur, adipiscing elit cum viverra vivamus diam, sem tempor luctus primis. Morbi himenaeos vulputate orci pretium nostra in turpis convallis, interdum condimentum lacinia rhoncus a volutpat ullamcorper, taciti parturient placerat elementum hac quis curae. Cum torquent ultrices enim magnis aptent senectus gravida cubilia sed, sodales placerat platea class quam mus viverra pulvinar etiam, euismod donec porta nibh ac lacus leo nec.
+        <div>
+        Lorem ipsum dolor sit amet consectetur, adipiscing elit cum viverra vivamus diam, sem tempor luctus primis. Morbi himenaeos vulputate orci pretium nostra in turpis convallis, interdum condimentum lacinia rhoncus a volutpat ullamcorper, taciti parturient placerat elementum hac quis curae. Cum torquent ultrices enim magnis aptent senectus gravida cubilia sed, sodales placerat platea class quam mus viverra pulvinar etiam, euismod donec porta nibh ac lacus leo nec.
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
+      <div class="col-lg-12">
+      Su buena anchura –en torno a los 141 centímetros– y una gran altura libre al techo –alrededor de 101 centímetros– hacen que
+      los dos ocupantes delanteros gocen de un gran espacio. Detrás, las cotas son inferiores. Sobre todo en la altura libre al
+      techo, una medida donde este A5 Sportback sólo ofrece 90 centímetros. Esto hace que las plazas traseras estén destinadas
+      principalmente a niños. A cambio, tenemos dos nuevas cualidades que son positivas. La primera es la existencia de puertas
+      traseras, característica que facilita enormemente entrar y salir del vehículo. Y la segunda es el comodísimo portón que da
+      acceso a un gran maletero. En este sentido, la capacidad de carga del A5 Sportback es buena –480 litros– y, además, bajo el
+      piso del mismo existe una rueda de repuesto.
       </div>
     </div>
   </section>

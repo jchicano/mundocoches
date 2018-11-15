4a9734c2b2549da1f6a99368dcfec6ba8348a0f5
Jesús David Chicano Galindo <JesusD@pc179-10.14004580.14.andared.ced.junta-andalucia.es>
2018-11-15 10:44:37 +0100

    Añadidos archivos para el análisis del M2

---
+++ analisis/analisis-m2-competition.php
@@ -0,0 +1,214 @@
+<?php $CURRENT_PAGE = "Analisis extendido"; ?>
+<?php $PAGE_TITLE = "Análisis | BMW M2 Competition"; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include("../includes/head-tag-contents.php");?>
+  <link rel="stylesheet" href="../css/analisis/m2-competition.css">
+</head>
+<body>
+
+<?php include("../includes/navigation.php");?>
+
+<!-- CONTENIDO -->
+
+<body class="bg-light"><!--Modificado-->
+<header class="masthead-a5">
+    <div class="container d-flex h-100 align-items-center">
+      <div class="mx-auto text-center">
+        <h1 class="mx-auto my-0">Audi A5 Sportback</h1>
+        <h2 class="text-white-50 mx-auto mt-2 mb-5"></h2>
+        <a href="#scroll" class="js-scroll-trigger">
+          <i class="fa fa-chevron-circle-down" style="font-size:48px;color:gainsboro"></i>
+        </a>
+      </div>
+    </div>
+  </header>
+  <div class="container bg-light">
+
+  <section id="scroll" class="projects-section bg-light">
+    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
+      <div class="col-lg-12">
+        <h2>Diseño y calidad</h2>
+      </div>
+      <div class="col-lg-8">
+          <div class="contenedor-izda caja-texto">
+          
+          <p>
+            Cuando los fabricantes apuestan por el diseño en un coche, en numerosas ocasiones, el confort de marcha queda mermado.
+            Una línea de techo coupé, unas suspensiones incómodas… hacen que la experiencia de conducir un coche con carácter deportivo
+            no sea particularmente favorable para el día a día.
+          </p>
+          <p>
+            Sin embargo, <a href="../marcas/">Audi</a> ha demostrado que unir estas cualidades –combinar un diseño atractivo con un interior versátil y
+            cómodo– es completamente compatible. Y lo ha hecho con el Audi A5 Sportback, una variante coupé del A4 que se caracteriza
+            por sus cinco puertas –portón incluido– y porque es uno de los coupés más vendidos en España. Una vez que estamos dentro
+            del modelo, lo primero que nos llama la atención es que es razonablemente amplio, principalmente en las plazas delanteras.
+          </p>
+          </div>
+      </div>
+      <div class="col-lg-4">
+        <div>
+          Lorem ipsum dolor sit amet consectetur, adipiscing elit cum viverra vivamus diam, sem tempor luctus primis. Morbi himenaeos vulputate orci pretium nostra in turpis convallis, interdum condimentum lacinia rhoncus a volutpat ullamcorper, taciti parturient placerat elementum hac quis curae. Cum torquent ultrices enim magnis aptent senectus gravida cubilia sed, sodales placerat platea class quam mus viverra pulvinar etiam, euismod donec porta nibh ac lacus leo nec.
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
+      <div class="col-lg-12">
+        <h2>Exterior e interior</h2>
+      </div>
+      <div class="col-lg-12">
+        <p>
+          Su buena anchura –en torno a los 141 centímetros– y una gran altura libre al techo –alrededor de 101 centímetros– hacen que
+          los dos ocupantes delanteros gocen de un gran espacio. Detrás, las cotas son inferiores. Sobre todo en la altura libre al
+          techo, una medida donde este A5 Sportback sólo ofrece 90 centímetros. Esto hace que las plazas traseras estén destinadas
+          principalmente a niños.
+        </p>
+        <p>
+          A cambio, tenemos dos nuevas cualidades que son positivas. La primera es la existencia de puertas
+          traseras, característica que facilita enormemente entrar y salir del vehículo. Y la segunda es el comodísimo portón que da
+          acceso a un gran maletero. En este sentido, la capacidad de carga del A5 Sportback es buena –480 litros– y, además, bajo el
+          piso del mismo existe una rueda de repuesto.
+        </p>
+      </div>
+    </div>
+    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
+      <div class="col-lg-6 caja-texto">
+          <div class="card text-center w-100 "><!-- class:float-left -->
+            <img class="card-img-top" src="../img/analisis/a5_cuadro_instrumentos.jpg" alt="Cuadro de instrumentos digital">
+            <div class="card-body">
+              <p class="card-text">El cuadro de instrumentos completamente digital es muy recomendable.</p>
+            </div>
+          </div>
+        </div>
+      <div class="col-lg-6 contenedor-dcha">
+          <p>
+            El Audi A5 Sportback 2017 de pruebas monta un motor archiconocido: el <b>2.0 TDI de 190 CV</b>, con tracción delantera y
+            cambio automático DSG de siete velocidades. Una combinación que parece ganadora de primeras, al menos en la lista de ventas.
+            Al arrancar se puede observar que el motor no es todo lo refinado que la envoltura merece. En frío suena algo tosco, como la mayoría de motores
+            diésel del <a href="../marcas">Grupo Volkswagen</a>. Echo a rodar y la verdad es que el motor de primeras se siente bien:
+            ofrece 400 Nm desde solamente las 1.750 vueltas, así que la respuesta inicial es buena. Acelera de cero a cien en 7,9
+            segundos.
+            Su comportamiento dinámico tiene poco que reprochar. El Audi Drive Select cuenta con cinco modos de conducción: eficiente,
+            confort, automático, dinámico e individual.
+          </p>
+      </div>
+    </div>
+    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
+      <div class="col-lg-12 caja-texto">
+        <p>
+          Bajo mi punto de vista con el modo eficiente y el modo dinámico (también los
+          podrían llamar tranquilo y emocionante) sería suficiente. Es un coche que en autopista se siente glorioso, aplomado,
+          confortable y con una sensación de calidad general que conquista. En curvas no va nada mal, con un reglaje de la suspensión
+          que evita desagradables balanceos: no obstante, no se siente del todo cómodo y si vas muy al límite el subviraje hace algún
+          que otro amago. Si vas a hacer mucha curva, yo invertiría algo más de dinero en el sistema de tracción integral quattro.
+        </p>
+        <p>
+          Los asientos de la versión S-Line me encantan: agarran bien pero tienen un mullido realmente confortable. Son asientos para
+          hacer kilómetros sin parar, pero resuelven la papeleta si vas a subir a buen ritmo un puerto de montaña. El Audi Virtual
+          Cockpit se abre ante mi cuando enciendo el contacto. Todos los botones y ajustes son de una gran calidad y Audi ha apostado
+          claramente por la tecnología led en el interior: incluso la ranura de un puerto USB está iluminada con una agradable luz blanca. Da sensación de tecnología, de modernidad y de calidad.
+        </p>
+      </div>
+    </div>
+    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
+      <div class="col-lg-12">
+        <h2>Conclusión</h2>
+      </div>
+      <div class="col-lg-12">
+      <p>
+        Dentro del segmento de las berlinas de diseño coupé y tamaño medio, el Audi A5 Sportback es uno de
+        los modelos más interesantes que existen en la actualidad. Tiene un diseño muy deportivo, es amplio por dentro y tiene un
+        gran maletero, lo que le convierte en un vehículo versátil y, sobre todo, es un coche ideal para viajar en él. Un coupé que
+        supera a sus rivales –el <a href="../marcas/">Mercedes</a> Clase C Coupé y del <a href="../marcas/">BMW</a> Serie 4 Gran Coupé– en aspectos como refinamiento y elevado
+        confort de marcha. Muy recomendado.
+      </p>
+      </div>
+    </div>
+    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
+      <hr class="my-4 w-100">
+      <div class="col-lg-12">
+        <h3>Relacionado</h3>
+      </div>
+      <div class="col-lg-12">
+        <ul>
+          <li>
+            <a href="../noticias/">Ya se sabe el precio del Audi A3</a>
+          </li>
+          <li>
+            <a href="../analisis/">Más análisis...</a>
+          </li>
+        </ul>
+      </div>
+      
+    </div>
+    <!--   -->
+
+
+    <!--<div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
+      <div class="col-lg-12 w-50">
+        <div class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="../img/analisis/audi_a5_sportback.jpg" alt="Primera imagen">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="..." alt="Segunda imagen">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="..." alt="Tercera imagen">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>-->
+      </div>
+    </div>
+  </section>
+
+
+
+
+      <!--
+      <section id="scroll" class="projects-section bg-light">
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="../img/analisis/audi_a5_sportback.jpg" alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                    <h4 class="text-white">Titulo</h4>
+                    <p class="mb-0 text-white-50">ANALISISANALISISANALISISANALISISANALISISANALISISANALISISANALISISANALISISANALISISANALISIS</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      -->
+  </div>
+</body>
+
+<?php include("../includes/footer.php");?>
+
+<?php include("../includes/body-tag-contents.php");?>
+
+</body>
+
+</body>
+</html>

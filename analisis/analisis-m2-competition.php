1888e03c48b22a6a432d717972aad9ba510f3444
Jesús David Chicano Galindo <JesusD@pc179-10.14004580.14.andared.ced.junta-andalucia.es>
2018-11-16 14:07:55 +0100

    Añadida galería de imágenes en proceso de creación

--- analisis/analisis-m2-competition.php
+++ analisis/analisis-m2-competition.php
@@ -6,6 +6,10 @@
 <head>
   <?php include("../includes/head-tag-contents.php"); ?>
   <link rel="stylesheet" href="../css/analisis/m2-competition.css">
+
+  <link rel="stylesheet" href="../css/analisis/galeria-imagenes.css">
+
+
 </head>
 <body>
 
@@ -14,7 +18,7 @@
 <!-- CONTENIDO -->
 
 <body class="bg-light"><!--Modificado-->
-<header class="masthead-a5">
+  <header class="masthead-a5">
     <div class="container d-flex h-100 align-items-center">
       <div class="mx-auto text-center">
         <h1 class="mx-auto my-0">BMW M2 Competition</h1>
@@ -28,23 +32,14 @@
   <div class="container bg-light">
 
   <section id="scroll" class="projects-section bg-light">
-    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
+    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify caja-texto">
       <div class="col-lg-12">
-        <h2>Diseño y calidad</h2>
+        <h2>Mucho más que un deportivo</h2>
       </div>
-      <div class="col-lg-8">
-          <div class="contenedor-izda caja-texto">
-          
-            <p>
-              <a href="../marcas/bmw.php">BMW</a> ha cumplido con los pronósticos y ha rizado el rizo del BMW Serie 2 para dejarnos con un BMW M2 aún más potente, con un BMW M2 aún más deportivo. Llega el BMW M2 Competition, un BMW M2 con más de 400 caballos, un BMW M2 que hereda la mecánica de su hermano mayor, el motor de 6 cilindros del BMW M3 y del BMW M4. Hora de conocer todos los detalles del nuevo BMW M2 Competition, ¿qué nos ofrece el nuevo deportivo de BMW?
-            </p>
-            <p>
-              Sin embargo, <a href="../marcas/audi.php">Audi</a> ha demostrado que unir estas cualidades –combinar un diseño atractivo con un interior versátil y
-              cómodo– es completamente compatible. Y lo ha hecho con el Audi A5 Sportback, una variante coupé del A4 que se caracteriza
-              por sus cinco puertas –portón incluido– y porque es uno de los coupés más vendidos en España. Una vez que estamos dentro
-              del modelo, lo primero que nos llama la atención es que es razonablemente amplio, principalmente en las plazas delanteras.
-            </p>
-          </div>
+      <div class="col-lg-8 contenedor-izda">
+        <p>
+          <a href="../marcas/bmw.php">BMW</a> ha cumplido con los pronósticos y ha rizado el rizo del BMW Serie 2 para dejarnos con un BMW M2 aún más potente, con un BMW M2 aún más deportivo. Llega el BMW M2 Competition, un BMW M2 con más de 400 caballos, un BMW M2 que hereda la mecánica de su hermano mayor, el motor de 6 cilindros del BMW M3 y del BMW M4. Hora de conocer todos los detalles del nuevo BMW M2 Competition, ¿qué nos ofrece el nuevo deportivo de BMW?
+        </p>
       </div>
       <div class="col-lg-4">
         <div>
@@ -58,55 +53,35 @@
       </div>
       <div class="col-lg-12">
         <p>
-          Su buena anchura –en torno a los 141 centímetros– y una gran altura libre al techo –alrededor de 101 centímetros– hacen que
-          los dos ocupantes delanteros gocen de un gran espacio. Detrás, las cotas son inferiores. Sobre todo en la altura libre al
-          techo, una medida donde este A5 Sportback sólo ofrece 90 centímetros. Esto hace que las plazas traseras estén destinadas
-          principalmente a niños.
-        </p>
-        <p>
-          A cambio, tenemos dos nuevas cualidades que son positivas. La primera es la existencia de puertas
-          traseras, característica que facilita enormemente entrar y salir del vehículo. Y la segunda es el comodísimo portón que da
-          acceso a un gran maletero. En este sentido, la capacidad de carga del A5 Sportback es buena –480 litros– y, además, bajo el
-          piso del mismo existe una rueda de repuesto.
+          El BMW M2 Competition cuenta con una parrilla de mayores dimensiones para favorecer la refrigeración del propulsor y el nuevo splitter delantero ayuda a recircular el aire para seguir enfriando dicho propulsor. 
+          Junto a la parrilla de mayores dimensiones, el nuevo “labio” delantero y las nuevas llantas hay un acabado en negro brillante para ciertos detalles y aparece una nueva insignia así como dos nuevos colores, el Sunset Orange y el Hockenheim Silver, este último específico de este modelo. Un cambio especialmente relevante es la aparición de unos nuevos retrovisores, con el diseño clásico de los BMW M3.
         </p>
       </div>
     </div>
-    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
-      <div class="col-lg-6 caja-texto">
-          <div class="card text-center w-100 "><!-- class:float-left -->
-            <img class="card-img-top" src="../img/analisis/a5_cuadro_instrumentos.jpg" alt="Cuadro de instrumentos digital">
+    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify caja-texto">
+      <div class="col-lg-6">
+        <div class="card text-center w-100 "><!-- class:float-left -->
+            <img class="card-img-top" src="../img/analisis/m2_interior.jpg" alt="Cuadro de instrumentos digital">
             <div class="card-body">
-              <p class="card-text">El cuadro de instrumentos completamente digital es muy recomendable.</p>
+              <p class="card-text">El habitáculo interior está muy bien rematado.</p>
             </div>
           </div>
-        </div>
-      <div class="col-lg-6 contenedor-dcha">
-          <p>
-            El Audi A5 Sportback 2017 de pruebas monta un motor archiconocido: el <b>2.0 TDI de 190 CV</b>, con tracción delantera y
-            cambio automático DSG de siete velocidades. Una combinación que parece ganadora de primeras, al menos en la lista de ventas.
-            Al arrancar se puede observar que el motor no es todo lo refinado que la envoltura merece. En frío suena algo tosco, como la mayoría de motores
-            diésel del <a href="../marcas/volkswagen.php">Grupo Volkswagen</a>. Echo a rodar y la verdad es que el motor de primeras se siente bien:
-            ofrece 400 Nm desde solamente las 1.750 vueltas, así que la respuesta inicial es buena. Acelera de cero a cien en 7,9
-            segundos.
-            Su comportamiento dinámico tiene poco que reprochar. El Audi Drive Select cuenta con cinco modos de conducción: eficiente,
-            confort, automático, dinámico e individual.
-          </p>
       </div>
-    </div>
-    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
-      <div class="col-lg-12 caja-texto">
+      <div class="col-lg-6 contenedor-dcha">
         <p>
-          Bajo mi punto de vista con el modo eficiente y el modo dinámico (también los
-          podrían llamar tranquilo y emocionante) sería suficiente. Es un coche que en autopista se siente glorioso, aplomado,
-          confortable y con una sensación de calidad general que conquista. En curvas no va nada mal, con un reglaje de la suspensión
-          que evita desagradables balanceos: no obstante, no se siente del todo cómodo y si vas muy al límite el subviraje hace algún
-          que otro amago. Si vas a hacer mucha curva, yo invertiría algo más de dinero en el sistema de tracción integral quattro.
+          Este coche recurre al motor del BMW M4, de 6 cilindros en línea, de doble turbo y de 3 litros de cubicaje, para dar vida a este BMW M2 más deportivo, proponiéndonos una potencia de <b>410 caballos</b> entre 5.250 rpm y 7.000 rpm y un par de 550 Nm disponible entre las 2.350 rpm y las 5.200 rpm.
+          Para poner en contexto esta potencia debemos tener en cuenta que el BMW M2 “convencional” desarrolla una potencia de 370 caballos con un par de 465 Nm, el BMW M4, como el M3, desarrolla 431 caballos y un par de 550 Nm.
         </p>
         <p>
-          Los asientos de la versión S-Line me encantan: agarran bien pero tienen un mullido realmente confortable. Son asientos para
-          hacer kilómetros sin parar, pero resuelven la papeleta si vas a subir a buen ritmo un puerto de montaña. El Audi Virtual
-          Cockpit se abre ante mi cuando enciendo el contacto. Todos los botones y ajustes son de una gran calidad y Audi ha apostado
-          claramente por la tecnología led en el interior: incluso la ranura de un puerto USB está iluminada con una agradable luz blanca. Da sensación de tecnología, de modernidad y de calidad.
+          El nuevo BMW M2 Competition sigue siendo un coche de propulsión, de tracción trasera y podemos configurarlo tanto con cambio manual como con el cambio automático DCT M de doble embrague y 7 velocidades, marcando un 0 a 100 km/h de 4.2 segundos en el caso del automático y de 4.4 segundos con el cambio manual, con un consumo medio de entre 9.2 y 10 l/100 km en función de la configuración.
+        </p>
+        
+      </div>
+    </div>
+    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify caja-texto">
+      <div class="col-lg-12">
+        <p>
+          En el interior encontramos nuevos umbrales para las puertas, unos asientos tipo baquet opcionales, el botón de arranque pasa a ser rojo y hay un nuevo acabado para los textiles del habitáculo, con Alcántara, con costuras contrastadas en azul o naranja y el logo de BMW M se encuentra ahora presente en los retrovisores.
         </p>
       </div>
     </div>
@@ -115,15 +90,112 @@
         <h2>Conclusión</h2>
       </div>
       <div class="col-lg-12">
-      <p>
+      <!--<p>
         Dentro del segmento de las berlinas de diseño coupé y tamaño medio, el Audi A5 Sportback es uno de
         los modelos más interesantes que existen en la actualidad. Tiene un diseño muy deportivo, es amplio por dentro y tiene un
         gran maletero, lo que le convierte en un vehículo versátil y, sobre todo, es un coche ideal para viajar en él. Un coupé que
         supera a sus rivales –el <a href="../marcas/mercedes.php">Mercedes</a> Clase C Coupé y del <a href="../marcas/bmw.php">BMW</a> Serie 4 Gran Coupé– en aspectos como refinamiento y elevado
         confort de marcha. Muy recomendado.
+      </p>-->
+      <p>
+
       </p>
       </div>
     </div>
+    <!-- INICIO GALERIA -->
+    <div class="container">
+	<div class="row"><!--
+
+Photos from unsplash.com
+
+Follow me on
+Dribbble: https://dribbble.com/supahfunk
+Twitter: https://twitter.com/supahfunk
+Codepen: https://codepen.io/supah/
+
+-->
+<div class="col-lg-12">
+<div class="gallery">
+  <figure>
+    <img src="https://images.unsplash.com/photo-1448814100339-234df1d4005d?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600" alt="" />
+    <figcaption>Daytona Beach <small>United States</small></figcaption>
+  </figure>
+  <figure>
+    <img src="https://images.unsplash.com/photo-1443890923422-7819ed4101c0?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600" alt="" />
+    <figcaption>Териберка, gorod Severomorsk <small>Russia</small></figcaption>
+  </figure>
+  <figure>
+    <img src="https://images.unsplash.com/photo-1445964047600-cdbdb873673d?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600" alt="" />
+    <figcaption>
+      Bad Pyrmont <small>Deutschland</small>
+    </figcaption>
+  </figure>
+  <figure>
+    <img src="https://images.unsplash.com/photo-1439798060585-62ab242d7724?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600" alt="" />
+    <figcaption>Yellowstone National Park <small>United States</small></figcaption>
+  </figure>
+  <figure>
+    <img src="https://images.unsplash.com/photo-1440339738560-7ea831bf5244?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600" alt="" />
+    <figcaption>Quiraing, Portree <small>United Kingdom</small></figcaption>
+  </figure>
+  <figure>
+    <img src="https://images.unsplash.com/photo-1441906363162-903afd0d3d52?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600" alt="" />
+    <figcaption>Highlands <small>United States</small></figcaption>
+  </figure>
+</div>
+</div>
+
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="display:none;">
+  <symbol id="close" viewBox="0 0 18 18">
+    <path fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M9,0.493C4.302,0.493,0.493,4.302,0.493,9S4.302,17.507,9,17.507
+			S17.507,13.698,17.507,9S13.698,0.493,9,0.493z M12.491,11.491c0.292,0.296,0.292,0.773,0,1.068c-0.293,0.295-0.767,0.295-1.059,0
+			l-2.435-2.457L6.564,12.56c-0.292,0.295-0.766,0.295-1.058,0c-0.292-0.295-0.292-0.772,0-1.068L7.94,9.035L5.435,6.507
+			c-0.292-0.295-0.292-0.773,0-1.068c0.293-0.295,0.766-0.295,1.059,0l2.504,2.528l2.505-2.528c0.292-0.295,0.767-0.295,1.059,0
+			s0.292,0.773,0,1.068l-2.505,2.528L12.491,11.491z"/>
+  </symbol>
+</svg>
+<script>
+    popup = {
+  init: function(){
+    $('figure').click(function(){
+      popup.open($(this));
+    });
+    
+    $(document).on('click', '.popup img', function(){
+      return false;
+    }).on('click', '.popup', function(){
+      popup.close();
+    })
+  },
+  open: function($figure) {
+    $('.gallery').addClass('pop');
+    $popup = $('<div class="popup" />').appendTo($('body'));
+    $fig = $figure.clone().appendTo($('.popup'));
+    $bg = $('<div class="bg" />').appendTo($('.popup'));
+    $close = $('<div class="close"><svg><use xlink:href="#close"></use></svg></div>').appendTo($fig);
+    $shadow = $('<div class="shadow" />').appendTo($fig);
+    src = $('img', $fig).attr('src');
+    $shadow.css({backgroundImage: 'url(' + src + ')'});
+    $bg.css({backgroundImage: 'url(' + src + ')'});
+    setTimeout(function(){
+      $('.popup').addClass('pop');
+    }, 10);
+  },
+  close: function(){
+    $('.gallery, .popup').removeClass('pop');
+    setTimeout(function(){
+      $('.popup').remove()
+    }, 100);
+  }
+}
+
+popup.init()
+
+</script>
+		
+	</div>
+</div>
+    <!-- FIN GALERIA -->
     <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
       <hr class="my-4 w-100">
       <div class="col-lg-12">
@@ -175,30 +247,6 @@
       </div>
     </div>
   </section>
-
-
-
-
-      <!--
-      <section id="scroll" class="projects-section bg-light">
-        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
-          <div class="col-lg-6">
-            <img class="img-fluid" src="../img/analisis/audi_a5_sportback.jpg" alt="">
-          </div>
-          <div class="col-lg-6">
-            <div class="bg-black text-center h-100 project">
-              <div class="d-flex h-100">
-                <div class="project-text w-100 my-auto text-center text-lg-left">
-                    <h4 class="text-white">Titulo</h4>
-                    <p class="mb-0 text-white-50">ANALISISANALISISANALISISANALISISANALISISANALISISANALISISANALISISANALISISANALISISANALISIS</p>
-                  <hr class="d-none d-lg-block mb-0 ml-0">
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      -->
   </div>
 </body>
 

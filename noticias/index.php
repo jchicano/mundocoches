739f2a502d007796110f938ff3fef34eaeedc7e5
unknown <jois_juas@hotmail.com>
2018-11-29 12:14:42 +0100

    Añadida imagen y noticia sin acabar

--- noticias/index.php
+++ noticias/index.php
@@ -21,6 +21,105 @@
             <div class="bg-black text-center h-100 project">
               <div class="d-flex h-100">
                 <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white">Mercedes-Benz Clase B, desvelado en París</h4>
+                  <p class="mb-0 text-white-50">Example.</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!--Noticia 2 -->
+      <section id="projects" class="projects-section bg-light">
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="../img/analisis/audi_a5_sportback.jpg" alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white">NOTICIAS</h4>
+                  <p class="mb-0 text-white-50">Example.</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--Noticia 3 -->
+      <section id="projects" class="projects-section bg-light">
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="../img/analisis/audi_a5_sportback.jpg" alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white">NOTICIAS</h4>
+                  <p class="mb-0 text-white-50">Example.</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--Noticia 4 -->
+      <section id="projects" class="projects-section bg-light">
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="../img/analisis/audi_a5_sportback.jpg" alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white">NOTICIAS</h4>
+                  <p class="mb-0 text-white-50">Example.</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--Noticia 5 -->
+      <section id="projects" class="projects-section bg-light">
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="../img/analisis/audi_a5_sportback.jpg" alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white">NOTICIAS</h4>
+                  <p class="mb-0 text-white-50">Example.</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--Noticia 6 -->
+      <section id="projects" class="projects-section bg-light">
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="../img/analisis/audi_a5_sportback.jpg" alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
                   <h4 class="text-white">NOTICIAS</h4>
                   <p class="mb-0 text-white-50">Example.</p>
                   <hr class="d-none d-lg-block mb-0 ml-0">

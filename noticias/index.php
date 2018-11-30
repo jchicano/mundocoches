632fee880d84ef1af0699316d46001a7c365c920
unknown <jois_juas@hotmail.com>
2018-11-30 09:30:39 +0100

    Noticia e Imagen nueva

--- noticias/index.php
+++ noticias/index.php
@@ -91,14 +91,14 @@
       <!--Noticia 5 -->
         <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
           <div class="col-lg-6">
-            <img class="img-fluid" src="../img/analisis/audi_a5_sportback.jpg" alt="">
+            <img class="img-fluid" src="../img/noticias/vw-touran.jpg" alt="">
           </div>
           <div class="col-lg-6">
             <div class="bg-black text-center h-100 project">
               <div class="d-flex h-100">
                 <div class="project-text w-100 my-auto text-center text-lg-left">
-                  <h4 class="text-white">NOTICIAS</h4>
-                  <p class="mb-0 text-white-50"><a href="noticia-hyundai.php">Click aquí para entrar en la noticia</a></p>
+                  <h4 class="text-white">Volkswagen Touran, ¿cuál es el más barato?</h4>
+                  <p class="mb-0 text-white-50"><a href="noticia-touran.php">Click aquí para entrar en la noticia</a></p>
                   <hr class="d-none d-lg-block mb-0 ml-0">
                 </div>
               </div>
---
+++ noticias/noticia-touran.php
@@ -0,0 +1,116 @@
+<?php $CURRENT_PAGE = "Noticia 3"; ?>
+<?php $PAGE_TITLE = "Noticia Hyundai"; ?>
+
+<?php include("../includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include("../includes/head-tag-contents.php");?>
+    <link href="../css/grayscale.css" rel="stylesheet">
+    <link href="../css/normalize.css" rel="stylesheet">
+</head>
+
+<?php include("../includes/navigation.php");?>
+
+<!-- CONTENIDO -->
+
+<body class="bg-light" id="page-top">
+<section id="scroll" class="projects-section bg-light">
+    <header class="masthead-a5">
+        <div class="container d-flex h-100 align-items-center">
+        <div class="mx-auto text-center">
+            <h1 class="mx-auto my-0">Volkswagen Touran, ¿cuál es el más barato?</h1>
+            <a href="#scroll" class="js-scroll-trigger">
+            <i class="fa fa-chevron-circle-down" style="font-size:48px;color:gainsboro"></i>
+            </a>
+        </div>
+        </div>
+    </header>
+</section>
+        <section id="scroll">
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify caja-texto">                   
+        <div class="col-lg-6 contenedor-izda">
+        <p>
+        El precio es uno de los factores decisivos de compra a la hora de adquirir un nuevo coche. Las razones son obvias, puedes tener lo que puedes pagar,
+        por lo que es ridículo optar a un vehículo que se nos escapa del presupuesto. Hoy quiero analizar el monovolumen de Volkswagen por si es una de tus
+        posibles próximas compras. Por ello vamos a dar respuesta a esta pregunta: ¿cuál es el Volkswagen Touran más barato que puedes comprar?
+        </p>
+                <div class="mx-auto col-lg-8 contenedor-izda">
+                <div class="card text-center w-100 ">
+                    <img class="card-img-top" src="../img/noticias/vw-golf.jpg" alt="">
+                    <div class="card-body">
+                    <p class="card-subtitle">Volkswagen Golf</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify caja-texto">
+        <div class="col-lg-6 contenedor-izda">
+        <p>
+        El Volkswagen Touran es el monovolumen estrella de la compañía de Wolfburgo, un vehículo diseñado para las familias que demandan los servicios de un monovolumen 
+        versátil y de gran maletero, con cinco o siete plazas. Está disponible con una completa familia de propulsores diésel y gasolina, mientras que también incluye 
+        toda la tecnología y los avances presentes en modelos superventas de la marca como el Volkswagen Golf. Ahora bien, ¿cuál es el más barato?
+        </p>
+            
+                <div class="mx-auto col-lg-8 contenedor-izda">
+                <div class="card text-center w-100 ">
+                    <img class="card-img-top" src="../img/noticias/vwEdition.jpg" alt="">
+                    <div class="card-body">
+                    <p class="card-subtitle">Volkswagen Edition</p>
+                    </div>
+                </div>
+                </div>
+        </div>
+        </div>
+
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify caja-texto">
+        <div class="col-lg-6 contenedor-izda">
+        <p>
+        
+        Por precio, ¿cuál es el Volkswagen Touran más asequible que puedes encontrar? Parte de los 27.100 euros (o 365,13 euros al mes) y, por esta cantidad, 
+        te llevas un Touran Edition 1.2 TSI de 110 CV y cambio manual de seis velocidades. Por 950 euros más podrás llevarte el motor 1.6 TDI de 115 CV con cambio manual.
+
+        El Paquete Edition España ofrece llantas Trondheim de 16 pulgadas, faros antiniebla delanteros con luz de giro, radio CD ‘Composition Media’ 
+        con reproducción MP3 y ocho altavoces, y reposabrazos central delantero. Pese a que te obliga a pagar, tiene una campaña promocional que descuenta el importe del paquete de equipamiento.
+        </p>
+
+        </div>
+        </div>
+    </section>
+
+        
+
+
+
+    
+
+</body>
+<?php include("../includes/footer.php");?>
+<?php include("../includes/body-tag-contents.php");?>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

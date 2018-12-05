dec5598353f9aded4526fa1ba142dfeeea6c4263
daviddar13 <pese1313@gmail.com>
2018-12-05 10:50:25 +0100

    acc

--- accesorios/aceite.php
+++ accesorios/aceite.php
@@ -37,7 +37,7 @@
   <header class="masthead-pd">
     <div class="container d-flex h-100 align-items-center">
       <div class="mx-auto text-center">
-        <h1 class="mx-auto my-0">Cambio de aceite</h1>
+        <h1 class="mx-auto my-0 bajar-tamano-movil">Cambio de aceite</h1>
         <h2 class="text-white-50 mx-auto mt-2 mb-5"></h2>
         <a href="#scroll" class="js-scroll-trigger">
           <i class="fa fa-chevron-circle-down" style="font-size:48px;color:gainsboro"></i>
--- accesorios/soporte.php
+++ accesorios/soporte.php
@@ -34,7 +34,7 @@
   <header class="masthead-pd">
     <div class="container d-flex h-100 align-items-center">
       <div class="mx-auto text-center">
-        <h1 class="mx-auto my-0">Soportes para móviles</h1>
+        <h1 class="mx-auto my-0 bajar-tamano-movil">Soportes para móviles</h1>
         <h2 class="text-white-50 mx-auto mt-2 mb-5"></h2>
         <a href="#scroll" class="js-scroll-trigger">
           <i class="fa fa-chevron-circle-down" style="font-size:48px;color:gainsboro"></i>

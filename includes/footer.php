eab45514f950ff63855be06077fc0d17594c6838
Jesús David Chicano Galindo <JesusD@pc179-10.14004580.14.andared.ced.junta-andalucia.es>
2018-12-12 13:00:57 +0100

    Añadida politica de privacidad

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <footer class="bg-black small text-center text-white-50">
   <div class="container">
-    Copyright &copy; MundoCoches 2018
+    Copyright &copy; MundoCoches 2018. <a href="politica.php">Política de privacidad</a> | <a href="#">Términos legales</a>
   </div>
 </footer>

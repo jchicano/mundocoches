e468530e94acee6f60fd0ab6fb46e3608c87569d
Jesús David Chicano Galindo <JesusD@MacBook-Pro-de-Jesus.local>
2018-12-23 09:57:03 +0100

    Añadido apartado para incluir los juegos. Se accede desde el footer.

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer class="bg-black small text-center text-white-50">
   <div class="container">
-    <p>Copyright &copy; MundoCoches 2018.</p>
+    <p>Copyright &copy; <a href="juegos/">MundoCoches</a> 2018.</p>
     <p><a href="politica.php">Política de privacidad</a> | <a href="#">Términos legales</a></p>
   </div>
 </footer>
---
+++ juegos/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta name="robots" content="noindex">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Juegos</title>
+</head>
+<body>
+    <ul>
+        <li>
+            <a href="">Jesús D.</a>
+        </li>
+        <li>
+            <a href="">Jaime C.</a>
+        </li>
+        <li>
+            <a href="">David A.</a>
+        </li>
+        <li>
+            <a href="">David R.</a>
+        </li>
+    </ul>
+</body>
+</html>

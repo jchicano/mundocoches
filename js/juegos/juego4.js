b6536cc2a460654eb576f21bbf0895fe0287ac55
davidruiz120 <davidruiz.99@hotmail.com>
2019-01-16 18:20:07 +0100

    Cambio juego DR

--- js/juegos/juego4.js
+++ js/juegos/juego4.js
@@ -1,123 +1,166 @@
-// === Variables ===
+//Variables globales
+var velocidad = 80;
+var tamano = 10;
 
-var bucle; // Guarda el bucle del juego
-var velocidad = 5; // Desplazamiento de los objetos en pixeles 
-var canvas = document.getElementById("canvas"); //Acceso al id canvas
-var areaW = canvas.width; // Ancho del canvas
-var areaH = canvas.height; // Alto del canvas
-var ctx = canvas.getContext("2d"); // Contexto gráfico, donde se dibuja
-var puntosj1 = 0; // Puntuación Jugador 1
-var puntosj2 = 0;  // Puntuación Jugador 2
-var tamanoPala = 75; // El tamaño de la pala
-var superficie = areaH - tamanoPala; // La superficie del área de juego
-
-// === Clases ===
-
-class Base { // Clase para detectar los choques
-    choque(obj){ 
-        if(this.fondo < obj.y || this.y > obj.fondo || this.derecha < obj.x || this.x > obj.derecha){
-            return false;
-        } else {
-            return true; // Hay contacto/colisión
-        }
-    }
+class objeto {
+	constructor(){
+		this.tamano = tamano;
+	}
+	choque(obj){
+		var difx = Math.abs(this.x - obj.x);
+		var dify = Math.abs(this.y - obj.y);
+		if(difx >= 0 && difx < tamano && dify >= 0 && dify < tamano){
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
 
-class Bola extends Base {
-    constructor(){
-        super();
-        this.t = 25; // Tamaño de la bola
-        this.x = Math.floor(Math.random() * (areaW - this.t)); // floor redondea
-        this.y = Math.floor(Math.random() * (areaH - this.t));
-        this.xdir = velocidad; // Variables de dirección
-        this.ydir = velocidad;
-    }
-
-    dibujar(){
-        ctx.fillRect(this.x, this.y, this.t, this.t); // X, Y, Ancho, Alto
-        ctx.fillStyle = '#000099';
-    }
-
-    choqueV(){ // Colisión vertical
-        if(this.y <= 0 || this.y >= (areaH - this.t)){
-            this.ydir = -this.ydir; // Cambia de dirección
-        }
-    }
-
-    choqueH(){ // Colisión horizontal
-        if(this.x <= 0){
-            this.xdir = this.xdir // Cambia de dirección
-            puntosj2++;
-        }
-        if(this.x >= (areaW - this.t)){
-            this.xdir = this.xdir // Cambia de dirección
-            puntosj1++;
-        }
-    }
-
-    mover(){ // Se suma constantemente la posición de X e Y y se comprueban las colisiones
-        this.x += this.xdir;
-        this.y += this.ydir;
-        this.choqueV();
-        this.choqueH();
-    }
+class Cola extends objeto {
+	constructor(x,y){
+		super();
+		this.x = x;
+		this.y = y;
+		this.siguiente = null;
+	}
+	dibujar(ctx){
+		if(this.siguiente != null){
+			this.siguiente.dibujar(ctx);
+		}
+		ctx.fillStyle = "#0000FF";
+		ctx.fillRect(this.x, this.y, this.tamano, this.tamano);
+	}
+	setxy(x,y){
+		if(this.siguiente != null){
+			this.siguiente.setxy(this.x, this.y);
+		}
+		this.x = x;
+		this.y = y;
+	}
+	meter(){
+		if(this.siguiente == null){
+			this.siguiente = new Cola(this.x, this.y);
+		} else {
+			this.siguiente.meter();
+		}
+	}
+	verSiguiente(){
+		return this.siguiente;
+	}
 }
 
-class Pala extends Base {
-    constructor(x){
-        super();
-        this.x = x;
-        this.y = Math.floor(Math.random() * superficie);
-        this.w = 25;
-        this.h = tamanoPala;
-        this.dir = 0;
-    }
-
-    dibujar() {
-        ctx.fillRect(this.x, this.y, this.w, this.h);
-    }
-
-    mover(){
-       this.y += this.dir;
-       if(this.y <= 0){ // Cuando llegue a lo alto
-           this.y = 0;
-           this.dir = 0;
-       } 
-       if(this.y >= superficie){ // Cuando llegue abajo
-           this.y = superficie;
-           this.dir = 0;
-       }
-    }
+class Comida extends objeto {
+	constructor(){
+		super();
+		this.x = this.generar();
+		this.y = this.generar();
+	}
+	generar(){
+		var num = (Math.floor(Math.random() * 59))*10;
+		return num;
+	}
+	colocar(){
+		this.x = this.generar();
+		this.y = this.generar();
+	}
+	dibujar(ctx){
+		ctx.fillStyle = "#FF0000";
+		ctx.fillRect(this.x, this.y, this.tamano, this.tamano);
+	}
+}
+//Objetos del juego
+var cabeza = new Cola(20,20);
+var comida = new Comida();
+var ejex = true;
+var ejey = true;
+var xdir = 0;
+var ydir = 0;
+function movimiento(){
+	var nx = cabeza.x+xdir;
+	var ny = cabeza.y+ydir;
+	cabeza.setxy(nx,ny);
+}
+function control(event){
+    var cod = event.keyCode;
+    console.log(cod);
+	if(ejex){
+		if(cod == 38){
+			ydir = -tamano;
+			xdir = 0;
+			ejex = false;
+			ejey = true;
+		}
+		if(cod == 40){
+			ydir = tamano;
+			xdir = 0;
+			ejex = false;
+			ejey = true;
+		}
+	}
+	if(ejey){
+		if(cod == 37){
+			ydir = 0;
+			xdir = -tamano;
+			ejey = false;
+			ejex = true;
+		}
+		if(cod == 39){
+			ydir = 0;
+			xdir = tamano;
+			ejey = false;
+			ejex = true;
+		}
+	}
 }
 
-// === Objetos ===
-
-var bola = new Bola();
-var jugador1 = new Pala(30); // Ese 30 es para separarlo de la pared izquierda
-var jugador2 = new Pala(545); // 600-25-30 | 600 = Ancho del área de juego 
-
-
-// === Funciones principales ===
+function findeJuego(){
+	xdir = 0;
+	ydir = 0;
+	ejex = true;
+	ejey = true;
+	cabeza = new Cola(20,20);
+	comida = new Comida();
+	alert("Perdiste");
+}
+function choquepared(){
+	if(cabeza.x < 0 || cabeza.x > 590 || cabeza.y < 0 || cabeza.y > 590){
+		findeJuego();
+	}
+}
+function choquecuerpo(){
+	var temp = null;
+	try{
+		temp = cabeza.verSiguiente().verSiguiente();
+	}catch(err){
+		temp = null;
+	}
+	while(temp != null){
+		if(cabeza.choque(temp)){
+			//fin de juego
+			findeJuego();
+		} else {
+			temp = temp.verSiguiente();
+		}
+	}
+}
 
 function dibujar(){
-    ctx.clearRect(0,0, areaW, areaH); // Borra lo anterior
-    bola.dibujar(); // Llama a la función del objeto para dibujar la bola
-    jugador1.dibujar();
-    jugador2.dibujar();
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(0,0, canvas.width, canvas.height);
+	//aquí abajo va todo el dibujo
+	cabeza.dibujar(ctx);
+	comida.dibujar(ctx);
 }
-
-function frame(){
-    bola.mover();
-    jugador1.mover();
-    jugador2.mover();
-    dibujar();
-    bucle = requestAnimationFrame(frame); // Hace una solicitud al navegador para realizar la animación, por lo que programará el repintado de la pantalla
-
+function main(){
+	choquecuerpo();
+	choquepared();
+	dibujar();
+	movimiento();
+	if(cabeza.choque(comida)){
+		comida.colocar();
+		cabeza.meter();
+	}
 }
-
-function start(){
-    var modal = document.getElementById("modal");
-    modal.style.display = "none"; // Al empezar oculta lo primero que se ve para iniciar el juego
-
-    frame(); // Para iniciar al juego, llamamos a frame para que haga la funcion de animar
-}
+setInterval("main()", velocidad);

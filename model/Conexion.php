f841f658c3071c5da66d38699b8d59b445d7e165
Jesús David Chicano Galindo <JesusD@MacBook-Pro-de-Jesus.local>
2019-01-23 18:02:01 +0100

    Añadida clase para realiar la conexion a la BD

---
+++ model/Conexion.php
@@ -0,0 +1,17 @@
+<?php
+class Conexion extends mysqli {
+    private $host = "localhost";
+    private $user = "b22_23036480";
+    private $pass = "DAWmundocoches321";
+    private $db = "b22_23036480_uno";
+    
+    
+    public function __construct() {
+        parent::__construct($this->host, $this->user, $this->pass, $this->db);
+        if(mysqli_connect_errno()){
+            echo mysqli_connect_error();
+            exit;
+        }
+    }
+}
+?>

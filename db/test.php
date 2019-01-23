72f957c6440db1536ad2383cca76221efe903fed
Jesús David Chicano Galindo <JesusD@MacBook-Pro-de-Jesus.local>
2019-01-23 21:11:33 +0100

    Añadidas senticas SQL

--- db/test.php
+++ db/test.php
@@ -1,9 +1,19 @@
-<?php
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
 require_once ('Conexion.php');
 require_once ('Usuario.php');
 
 $con = new Conexion();
+$con->set_charset("utf8"); //Establecemos la codificacion adecuada
 $sql = "SELECT * FROM usuario";
 $resultado = $con->query($sql);
 if($con->affected_rows){ //Devuelve 0 o un numero
@@ -18,15 +28,19 @@ if($con->affected_rows){ //Devuelve 0 o un numero
 //return FALSE;
 
 foreach ($usuarios as $usuario) {
-    echo $usuario->id + "<br>";
-    echo $usuario->email + "<br>";
-    echo $usuario->pass + "<br>";
-    echo $usuario->nombre + "<br>";
-    echo $usuario->apellido1 + "<br>";
-    echo $usuario->apellido1 + "<br>";
-    echo $usuario->fecha_nac + "<br>";
-    echo $usuario->pais + "<br>";
-    echo $usuario->cod_postal + "<br>";
-    echo $usuario->telefono + "<br>";
-    echo $usuario->rol + "<br>";
+    echo $usuario->id . "<br>";
+    echo $usuario->email . "<br>";
+    echo $usuario->pass . "<br>";
+    echo $usuario->nombre . "<br>";
+    echo $usuario->apellido1 . "<br>";
+    echo $usuario->apellido1 . "<br>";
+    echo $usuario->fecha_nac . "<br>";
+    echo $usuario->pais . "<br>";
+    echo $usuario->cod_postal . "<br>";
+    echo $usuario->telefono . "<br>";
+    echo $usuario->rol . "<br>";
 }
+
+    ?>
+</body>
+</html>

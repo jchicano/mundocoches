a83190ad9b13fd35fa030ba467bb28c470dc45f2
Jesús David Chicano Galindo <JesusD@MacBook-Pro-de-Jesus.local>
2019-01-24 21:47:11 +0100

    Añadido editor de texto Summernote

--- db/test.php
+++ db/test.php
@@ -6,13 +6,46 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 
-<!------------------------------------------->
+    <!----------------SCRIPTS PARA SUMMERNOTE CON BOOTSTRAP (NO BORRAR LOS DE BOOTSTRAP PORQUE PETA)--------------------->
+    <!-- Stuff necessary for summernote -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote-bs4.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote-bs4.js"></script>
+
+    <script>
+    $(document).ready(function() {
+        $('#summernote').summernote({
+            toolbar: [ //Personalizamos la barra de herramientas
+                // [groupName, [list of button]]
+                ['style', ['bold', 'italic', 'underline', 'clear']],
+                ['font', ['Segoe UI']],
+                ['fontsize', ['fontsize',16]],
+                ['color', ['color']],
+                ['para', ['ul', 'ol']],
+            ]
+        });
+        $('#summernote').summernote('fontName', 'Segoe UI'); // Establecemos la fuente de la guia de estilos
+        $('#summernote').summernote('justifyFull'); // Justificamos todo por defecto
+        $('#summernote').summernote({
+            disableDragAndDrop: true,    // Deshabilitamos el Drag & Drop
+            placeholder: 'write here...'// Custom placeholder
+        }); 
+
+        function limpiar(){
+            $('#summernote').summernote('code','');
+        }
+
+        //Para obtener el codigo HTML
+        //$('#summernote').summernote('code');
+
+        $('#boton').on("click",limpiar);
+
+    });
+    </script>
+
   </head>
 
 
@@ -53,38 +86,23 @@ foreach ($usuarios as $usuario) {
 
     ?>
 
+<div class="container">
 <div class="row">
 <div class="col-lg-12">
 <form method="post">
-  <textarea id="summernote" name="editordata"></textarea>
+  <textarea id="summernote" name="editordata" cols="30" rows="100"></textarea>
 </form>
 </div>
 </div>
-<button onclick="mostrar()">Mostrar</button>
+</div>
+<button id="boton">Limpiar</button>
 <br><br><br>
 <div id="cuadro" style="height:120px; width:210px; background-color: lightblue;"></div>
 
 
 
 
-    <script>
-    $('#summernote').summernote({
-        toolbar: [ //Personalizamos la barra de herramientas
-            // [groupName, [list of button]]
-            ['style', ['bold', 'italic', 'underline', 'clear']],
-            ['font', ['Segoe UI']],
-            ['fontsize', ['fontsize']],
-            ['color', ['color']],
-            ['para', ['ul', 'ol', 'paragraph']],
-        ]
-    });
-      $('#summernote').summernote('fontName', 'Segoe UI');
-
-    function mostrar(){
-        alert($('#summernote').summernote('code'));
-    }
-
-    </script>
+    
 
 </body>
 </html>

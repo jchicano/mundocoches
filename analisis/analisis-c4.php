dbb21e1115d77472f8306cd16059c1f91565e23f
Jesús David Chicano Galindo <JesusD@MacBook-Pro-de-Jesus.local>
2019-01-24 22:19:55 +0100

    Añadido análisis desde la BD

--- analisis/analisis-c4.php
+++ analisis/analisis-c4.php
@@ -1,228 +1,85 @@
-<?php $CURRENT_PAGE = "Analisis extendido"; ?>
-<?php $PAGE_TITLE = "Análisis | Citroën C4"; ?>
+<?php
+
+require_once("../db/Conexion.php");
+require_once("../db/Contenido.php");
+require_once("../db/Usuario.php");
+
+//Guardamos la URL de la página actual
+$url = $_SERVER["REQUEST_URI"];
+
+//Conexion y consulta
+$con = new Conexion();
+$con->set_charset("utf8"); //Establecemos la codificacion adecuada
+$sql = "SELECT *
+        FROM contenido
+        WHERE url='$url'";
+$resultado = $con->query($sql);
+if($con->affected_rows){ //Devuelve 0 o un numero
+  while($fila = $resultado->fetch_object()){
+      $c = new Contenido($fila->id, $fila->url, $fila->titulo, $fila->fecha_publicacion, $fila->id_usuario_autor, $fila->texto); //Nombre de las columnas de la tabla
+  }
+  $con->close();
+}
+
+$CURRENT_PAGE = "Analisis extendido";
+$PAGE_TITLE = $c->titulo; //Sacado de la bd el titulo
+
+
+$stringInicial = "/analisis/analisis-"; //String que queremos eliminar de la URL
+
+if(substr($url, 0, strlen($stringInicial)) === $stringInicial) //Calculamos la longitud de ese string y lo restamos
+    $str = substr($url, strlen($stringInicial));
+
+$str = basename($str, ".php"); //eliminamos la extension del archivo
+
+?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
   <?php include("../includes/head-tag-contents.php"); ?>
-  <link rel="stylesheet" href="../css/analisis/c4.css">
+  <link rel="stylesheet" href="../css/analisis/<?php echo $str ?>.css">
 
   <link rel="stylesheet" href="../css/analisis/galeria-imagenes.css">
 
 </head>
 <body>
 
-<?php include("../includes/navigation.php"); ?>
+<!--Texto de la pagina-->
+<?php echo $c->texto;?>
 
-<!-- CONTENIDO -->
 
-<body class="bg-light"><!--Modificado-->
-  <header class="masthead-c4">
-    <div class="container d-flex h-100 align-items-center">
-      <div class="mx-auto text-center">
-        <h1 class="mx-auto my-0 bajar-tamano-movil">Citroën C4</h1>
-        <h2 class="text-white-50 mx-auto mt-2 mb-5"></h2>
-        <a href="#scroll" class="js-scroll-trigger">
-          <i class="fa fa-chevron-circle-down" style="font-size:48px;color:gainsboro"></i>
-        </a>
-      </div>
-    </div>
-  </header>
-  <div class="container bg-light">
-
-  <section id="scroll" class="projects-section bg-light">
-    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify caja-texto">
-      <div class="col-lg-12">
-        <h2>Todo un éxito</h2>
-      </div>
-      <div class="col-lg-8 contenedor-izda">
-        <p>
-          El Citroën C4 ha sido uno de los compactos más vendidos de los últimos años. De hecho, el año pasado recibió el Premio Estrella Luike, al haber conseguido alzarse como el modelo de más ventas en España.
-          No obstante, conviene señalar que, en el cómputo, se incluyeron las variantes C4 Picasso y Grand Picasso, así como el C4 Cactus, los cuales no comparten plataforma con el modelo C4 original.
-          La generación actual data del año 2010, si bien ha sido objeto de múltiples actualizaciones. La última tuvo lugar este mismo año 2016, lo cual le ha permitido mantenerse en la palestra pese al empuje de otros competidores como el <a href="../marcas/leon.php">Seat León</a>, <a href="../marcas/ibiza.php">Seat Ibiza</a> o <a href="../marcas/golf.php">Volkswagen Golf</a>.
-        </p>
-      </div>
-      <div class="col-lg-4">
-        <!--Tabla informativa-->
-        <table class="table table-sm">
-          <thead>
-            <tr>
-              <th class="text-center" colspan="2" scope="col">Citroën C4</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Combustible</td>
-              <td class="text-right">Diésel</td>
-            </tr>
-            <tr>
-              <td> Nº cilindros</td>
-              <td class="text-right">4</td>
-            </tr>
-            <tr>
-              <td>Cambio</td>
-              <td class="text-right">Manual</td>
-            </tr>
-            <tr>
-              <td>Nº plazas</td>
-              <td class="text-right">5</td>
-            </tr>
-            <tr>
-              <td>Tracción</td>
-              <td class="text-right">Delantera</td>
-            </tr>
-          </tbody>
-        </table>
-        <a href="../marcas/c4.php">Más información...</a>
-        <!---->
-      </div>
-    </div>
-    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
-      <div class="col-lg-12">
-        <h2>Exterior e interior</h2>
-      </div>
-      <div class="col-lg-12">
-        <p>
-          El Citroën C4 tiene una longitud de 4,329 metros. Su anchura es de 1,789 metros y alcanza una altura de 1,489 metros; ligeramente más largo que el Volkswagen Golf y un poco más corto que el <a href="../marcas/megane.php">Renault Mégane</a>. Es decir, es un compacto puro.
-          Ahora bien, cuenta con uno de los maleteros más grandes de su categoría, el cual dispone de 408 litros de capacidad, los cuales pueden ser ampliados hasta 1.183, abatiendo los asientos traseros en proporción 60:40.
-        </p>
-        <p>
-          En el diseño exterior destacan los faros delanteros de conducción diurna con LEDs, así como el efecto 3D de los faros traseros.
-          Las líneas del C4 son suaves y, de vez en cuando, encontramos elementos cromados, que contribuyen a aportar cierta elegancia.
-        </p>
-      </div>
-    </div>
-    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify caja-texto">
-      <div class="col-lg-6">
-        <div class="card text-center w-100 "><!-- class:float-left -->
-            <img class="card-img-top" src="../img/analisis/c4_interior.jpg" alt="Equipacion">
-            <div class="card-body">
-              <p class="card-text">Este coche viene muy bien equipado.</p>
-            </div>
-          </div>
-      </div>
-      <div class="col-lg-6 contenedor-dcha">
-        <p>
-          El salpicadero ha mantenido la misma línea práctica a lo largo de los últimos años.
-          Por un lado, cuenta con numerosos huecos portaobjetos, incluyendo los cajones bajos los asientos, la guantera iluminada, así como los espacios en las puertas y la propia consola central.
-        </p>
-        <p>
-          Precisamente, dicha consola fue objeto de recientes mejoras, mediante la eliminación de varios botones, para dar mayor protagonismo a la pantalla táctil de 7 pulgadas, con la cual se controla la navegación, la radio, el teléfono, así como múltiples ajustes posibles en el vehículo.
-          El cuadro de instrumentación del conductor combina elementos digitales y analógicos y un volante con mandos centralizados por zonas.
-          Las tapicerías están disponibles en diferentes telas y acabados, así como en cuero.
-        </p>
-      </div>
-    </div>
-    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify separar-arriba">
-      <div class="col-lg-12">
-        <p>
-          Este equipamiento incluye la ayuda a la salida en pendiente (Hill Assist), el sistema de alerta del ángulo muerto, el control de tracción inteligente, climatizador automático bi-zonal, sensores de lluvia y luminosidad, sensores de aparcamiento trasero, etc.
-          La conectividad también tiene presencia en la versión Feel Edition de este modelo, gracia al sistema Connect Box, el cual incluye una guía de mantenimiento virtual, el sistema de llamada de emergencia y un módulo de conducción ecológica. Esta versión también cuenta con acceso y arranque manos libres, y freno de mano eléctrico.
-          Por cierto, no podemos ignorar que en el acabado más alto, el asiento del conductor también incluye función con masaje, algo prácticamente insólito en los modelos de esta categoría.
-        </p>
-      </div>
-    </div>
-    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
-      <div class="col-lg-12">
-        <h2>Conclusión</h2>
-      </div>
-      <div class="col-lg-12">
-        <p>
-          Nos encontramos ante un coche redondo. De esos que no se venden, sino que se despachan, porque es una fenomenal compra a ciegas. Es el típico coche que la gente que no sabe de coches se compra por tranquilidad y seguridad... y la verdad es que aciertan. En este caso concreto, ya con ayuda de ofertas por estar cerca de su final comercial, el C4 puede seguir siendo el coche más vendido, o uno de los que más, pues no falla en nada, ni nada desentona en él. Cuenta con buenos motores, un espacio y una habitabilidad a prueba de pruebas, fácil de entender y conducir y, encima, su precio es muy coherente. Con estos mimbres, definitivamente, no es de extrañar que haya sido el coche más vendido en España en el período 2016/2017.
-        </p>
-      </div>
-    </div>
-    <hr class="my-4 w-100">
-    <!-- INICIO GALERIA -->
-    <div class="gallery">
-      <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
-        <div class="col-lg-4">
-          <figure>
-            <img src="../img/analisis/gallery/c4-1.jpg" alt="" />
-            <!--<figcaption>Daytona Beach <small>United States</small></figcaption>-->
-          </figure>
-        </div>
-        <div class="col-lg-4">
-          <figure>
-            <img src="../img/analisis/gallery/c4-2.jpg" alt="" />
-            <!--<figcaption>Russia, gorod Severomorsk <small>Russia</small></figcaption>-->
-          </figure>
-        </div>
-        <div class="col-lg-4">
-          <figure>
-            <img src="../img/analisis/gallery/c4-3.jpg" alt="" />
-            <!--<figcaption>Bad Pyrmont <small>Deutschland</small></figcaption>-->
-          </figure>
-        </div>
-      </div>
-      <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
-        <div class="col-lg-4">
-          <figure>
-            <img src="../img/analisis/gallery/c4-4.jpg" alt="" />
-            <!--<figcaption>Yellowstone National Park <small>United States</small></figcaption>-->
-          </figure>
-        </div>
-        <div class="col-lg-4">
-          <figure>
-            <img src="../img/analisis/gallery/c4-5.jpg" alt="" />
-            <!--<figcaption>Quiraing, Portree <small>United Kingdom</small></figcaption>-->
-          </figure>
-        </div>
-        <div class="col-lg-4">
-          <figure>
-            <img src="../img/analisis/gallery/c4-6.jpg" alt="" />
-            <!--<figcaption>Highlands <small>United States</small></figcaption>-->
-          </figure>
-        </div>
-      </div>
-
-      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="display:none;">
-        <symbol id="close" viewBox="0 0 18 18">
-          <path fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M9,0.493C4.302,0.493,0.493,4.302,0.493,9S4.302,17.507,9,17.507
-            S17.507,13.698,17.507,9S13.698,0.493,9,0.493z M12.491,11.491c0.292,0.296,0.292,0.773,0,1.068c-0.293,0.295-0.767,0.295-1.059,0
-            l-2.435-2.457L6.564,12.56c-0.292,0.295-0.766,0.295-1.058,0c-0.292-0.295-0.292-0.772,0-1.068L7.94,9.035L5.435,6.507
-            c-0.292-0.295-0.292-0.773,0-1.068c0.293-0.295,0.766-0.295,1.059,0l2.504,2.528l2.505-2.528c0.292-0.295,0.767-0.295,1.059,0
-            s0.292,0.773,0,1.068l-2.505,2.528L12.491,11.491z"/>
-        </symbol>
-      </svg>
-      
-      
-      
-    </div>
-    <!-- FIN GALERIA -->
-
+<!--Incluyo el autor del contenido-->
+<?php
+//Conexion y consulta
+$con = new Conexion();
+$con->set_charset("utf8"); //Establecemos la codificacion adecuada
+$sql = "SELECT *
+        FROM usuario,contenido
+        WHERE contenido.id_usuario_autor=usuario.id";
+$resultado = $con->query($sql);
+if($con->affected_rows){ //Devuelve 0 o un numero
+  while($fila = $resultado->fetch_object()){
+      $u = new Usuario($fila->id, $fila->email, $fila->pass, $fila->nombre, $fila->apellido1, $fila->apellido2, $fila->fecha_nac, $fila->pais, $fila->cod_postal, $fila->telefono, $fila->rol); //Nombre de las columnas de la tabla
+    }
+  $con->close();
+}
+?>
+<div class="container">
+  <section id="scroll" class="projects-section bg-light reducirMargenSuperior">
     <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
       <hr class="my-4 w-100">
       <div class="col-lg-12">
-        <h3>Relacionado</h3>
-      </div>
-      <div class="col-lg-12">
-        <ul>
-          <li>
-            <a href="../noticias/">Más noticias...</a>
-          </li>
-          <li>
-            <a href="../analisis/">Más análisis...</a>
-          </li>
-        </ul>
-      </div>
-      <hr class="my-4 w-100">
-      <div class="col-lg-12">
-        <h3>Fuente(s)</h3>
-      </div>
-      <div class="col-lg-12">
-        <ul>
-          <li>
-            <a href="https://www.leaseplango.es/blog/comparativa/citroen-c4-analisis-completo/">Enlace 1</a>
-          </li>
-          <li>
-            <a href="http://www.revistadelmotor.es/2017/02/25/prueba-citroen-c4.html/">Enlace 2</a>
-          </li>
-        </ul>
+        <h5>Autor: <?php echo $u->nombre ?></h5>
       </div>
     </div>
   </section>
-  </div>
+</div>
+
+
+<!--Resto de scripts-->
+<?php include("../includes/navigation.php"); ?>
 
 <?php include("../includes/gallery-script.php");?>
 
@@ -232,3 +89,4 @@
 
 </body>
 </html>
+

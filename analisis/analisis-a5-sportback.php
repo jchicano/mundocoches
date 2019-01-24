a03251fb964e17a6e05e77fe9d4b1658dbeae3e7
Jesús David Chicano Galindo <JesusD@pc179-10.14004580.14.andared.ced.junta-andalucia.es>
2019-01-24 10:02:07 +0100

    Analisis finalizado con BD

--- analisis/analisis-a5-sportback.php
+++ analisis/analisis-a5-sportback.php
@@ -4,7 +4,7 @@ require_once("../db/Conexion.php");
 require_once("../db/Contenido.php");
 
 //Guardamos la URL de la página actual
-$url = $_SERVER['REQUEST_URI'];
+$url = $_SERVER["REQUEST_URI"];
 
 //Conexion y consulta
 $con = new Conexion();
@@ -15,49 +15,44 @@ $sql = "SELECT *
 $resultado = $con->query($sql);
 if($con->affected_rows){ //Devuelve 0 o un numero
   while($fila = $resultado->fetch_object()){
-      $c = new Contenido($fila->id, $fila->url, $fila->titulo, $fila->fecha_publicacion, $fila->texto); //Nombre de las columnas de la tabla
+      $c = new Contenido($fila->id, $fila->url, $fila->titulo, $fila->fecha_publicacion, $fila->id_usuario_autor, $fila->texto); //Nombre de las columnas de la tabla
   }
   $con->close();
 }
 
 $CURRENT_PAGE = "Analisis extendido";
+$PAGE_TITLE = $c->titulo; //Sacado de la bd el titulo
 
-$tituloPagina = $c->titulo;
-
-
-$PAGE_TITLE = $tituloPagina; //Sacar de la bd el titulo
-
-//echo base64_decode($c->texto);
-
-//echo $c->texto;
+//$url devuelve /analisis/analisis-a5-sportback.php 
+$str = substr($url, -16); //eliminamos la primera parte de la ruta
+$str = basename($str, ".php"); //eliminamos la extension del archivo
 
 ?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
-  <?php include('../includes/head-tag-contents.php'); ?>
-  <link rel='stylesheet' href='../css/analisis/a5-sportback.css'>
+  <?php include("../includes/head-tag-contents.php"); ?>
+  <link rel="stylesheet" href="../css/analisis/<?php echo $str ?>.css">
 
-  <link rel='stylesheet' href='../css/analisis/galeria-imagenes.css'>
+  <link rel="stylesheet" href="../css/analisis/galeria-imagenes.css">
 
 </head>
 <body>
 
+<!--Texto de la pagina-->
 <?php echo $c->texto; ?>
 
-<?php include('../includes/navigation.php'); ?>
+<!--Resto de scripts-->
+<?php include("../includes/navigation.php"); ?>
 
-<?php include('../includes/gallery-script.php');?>
+<?php include("../includes/gallery-script.php");?>
 
-<?php include('../includes/footer.php');?>
+<?php include("../includes/footer.php");?>
 
-<?php include('../includes/body-tag-contents.php');?>
+<?php include("../includes/body-tag-contents.php");?>
 
 </body>
 </html>
-
-<?php
-//BD texto
-?>
 

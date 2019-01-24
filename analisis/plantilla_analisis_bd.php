a98a5b0cbc9b60a9931af5e372d0e9e418b79c0f
Jesús David Chicano Galindo <JesusD@pc179-10.14004580.14.andared.ced.junta-andalucia.es>
2019-01-24 09:42:59 +0100

    Añadida plantilla para los analisis de la BD

---
+++ analisis/plantilla_analisis_bd.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once("../db/Conexion.php");
+require_once("../db/Contenido.php");
+
+//Guardamos la URL de la página actual
+$url = $_SERVER['REQUEST_URI'];
+
+//Conexion y consulta
+$con = new Conexion();
+$con->set_charset("utf8"); //Establecemos la codificacion adecuada
+$sql = "SELECT *
+        FROM contenido
+        WHERE url='$url'";
+$resultado = $con->query($sql);
+if($con->affected_rows){ //Devuelve 0 o un numero
+  while($fila = $resultado->fetch_object()){
+      $c = new Contenido($fila->id, $fila->url, $fila->titulo, $fila->fecha_publicacion, $fila->texto); //Nombre de las columnas de la tabla
+  }
+  $con->close();
+}
+
+$CURRENT_PAGE = "Analisis extendido";
+$PAGE_TITLE = $c->titulo; //Sacado de la bd el titulo
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include('../includes/head-tag-contents.php'); ?>
+  <link rel='stylesheet' href='../css/analisis/a5-sportback.css'>
+
+  <link rel='stylesheet' href='../css/analisis/galeria-imagenes.css'>
+
+</head>
+<body>
+
+<!--Texto de la pagina-->
+<?php echo $c->texto; ?>
+
+<!--Resto de scripts-->
+<?php include('../includes/navigation.php'); ?>
+
+<?php include('../includes/gallery-script.php');?>
+
+<?php include('../includes/footer.php');?>
+
+<?php include('../includes/body-tag-contents.php');?>
+
+</body>
+</html>
+

f933c78c88026efc921c6f37930a62f32bb7bf74
Jesús David Chicano Galindo <JesusD@MacBook-Pro-de-Jesus.local>
2019-01-24 22:16:05 +0100

    Añadido analisis sacado de la BD

--- analisis/analisis-amg-c-43-4matic.php
+++ analisis/analisis-amg-c-43-4matic.php
@@ -1,234 +1,85 @@
-<?php $CURRENT_PAGE = "Analisis extendido"; ?>
-<?php $PAGE_TITLE = "Análisis | Mercedes AMG C43 4MATIC"; ?>
+<?php
+
+require_once("../db/Conexion.php");
+require_once("../db/Contenido.php");
+require_once("../db/Usuario.php");
+
+//Guardamos la URL de la página actual
+$url = $_SERVER["REQUEST_URI"];
+
+//Conexion y consulta
+$con = new Conexion();
+$con->set_charset("utf8"); //Establecemos la codificacion adecuada
+$sql = "SELECT *
+        FROM contenido
+        WHERE url='$url'";
+$resultado = $con->query($sql);
+if($con->affected_rows){ //Devuelve 0 o un numero
+  while($fila = $resultado->fetch_object()){
+      $c = new Contenido($fila->id, $fila->url, $fila->titulo, $fila->fecha_publicacion, $fila->id_usuario_autor, $fila->texto); //Nombre de las columnas de la tabla
+  }
+  $con->close();
+}
+
+$CURRENT_PAGE = "Analisis extendido";
+$PAGE_TITLE = $c->titulo; //Sacado de la bd el titulo
+
+
+$stringInicial = "/analisis/analisis-"; //String que queremos eliminar de la URL
+
+if(substr($url, 0, strlen($stringInicial)) === $stringInicial) //Calculamos la longitud de ese string y lo restamos
+    $str = substr($url, strlen($stringInicial));
+
+$str = basename($str, ".php"); //eliminamos la extension del archivo
+
+?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
   <?php include("../includes/head-tag-contents.php"); ?>
-  <link rel="stylesheet" href="../css/analisis/amg-c-43-4matic.css">
+  <link rel="stylesheet" href="../css/analisis/<?php echo $str ?>.css">
 
   <link rel="stylesheet" href="../css/analisis/galeria-imagenes.css">
 
 </head>
 <body>
 
-<?php include("../includes/navigation.php"); ?>
+<!--Texto de la pagina-->
+<?php echo $c->texto;?>
 
-<!-- CONTENIDO -->
 
-<body class="bg-light"><!--Modificado-->
-  <header class="masthead-c43">
-    <div class="container d-flex h-100 align-items-center">
-      <div class="mx-auto text-center">
-        <h1 class="mx-auto my-0 bajar-tamano-movil">Mercedes AMG C 43 4MATIC</h1>
-        <h2 class="text-white-50 mx-auto mt-2 mb-5"></h2>
-        <a href="#scroll" class="js-scroll-trigger">
-          <i class="fa fa-chevron-circle-down" style="font-size:48px;color:gainsboro"></i>
-        </a>
-      </div>
-    </div>
-  </header>
-  <div class="container bg-light">
-
-  <section id="scroll" class="projects-section bg-light">
-    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify caja-texto">
-      <div class="col-lg-12">
-        <h2>Más potente y mejor equipado que nunca</h2>
-      </div>
-      <div class="col-lg-8 contenedor-izda">
-        <p>
-          En <a href="../marcas/mercedes.php">Mercedes</a> se han dado cuenta de que, en ocasiones, los términos medios tienen
-          sentido y el C 43 4Matic Coupé se ha encargado de reiterarlo desde que llegó al mercado en 2016. Con la misma estética
-          que el salvaje C 63 de 476 CV y un comportamiento dinámico muy superior al del tranquilo C 300, el C 43 se ha convertido
-          en la alternativa ideal para aquellos que buscaban un <b>punto medio</b> entre la lógica y la locura; sobre todo ahora
-          que acaba de someterse a una pequeña actualización.
-        </p>
-      </div>
-      <div class="col-lg-4">
-        <!--Tabla informativa-->
-        <table class="table table-sm">
-          <thead>
-            <tr>
-              <th class="text-center" colspan="2" scope="col">Mercedes AMG C 43 MATIC</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Combustible</td>
-              <td class="text-right">Gasolina</td>
-            </tr>
-            <tr>
-              <td> Nº cilindros</td>
-              <td class="text-right">6</td>
-            </tr>
-            <tr>
-              <td>Cambio</td>
-              <td class="text-right">Automático</td>
-            </tr>
-            <tr>
-              <td>Nº plazas</td>
-              <td class="text-right">5</td>
-            </tr>
-            <tr>
-              <td>Tracción</td>
-              <td class="text-right">Integral</td>
-            </tr>
-          </tbody>
-        </table>
-        <a href="../marcas/amg_c43.php">Más información...</a>
-        <!---->
-      </div>
-    </div>
-    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
-      <div class="col-lg-12">
-        <h2>Interior y exterior</h2>
-      </div>
-      <div class="col-lg-12">
-        <p>
-          Basta con apretar el botón de Start para percartarse de los cambios; el V6 cobra vida con un bramido mucho más sonoro que
-          en el modelo precedente. Las unidades dispuestas por Mercedes-AMG para la toma de contacto incluyen el equipo de escape
-          AMG Performance –1.791 euros– y lo cierto es que suenan de maravilla, tanto que no tienen nada que envidiar al estruendoso
-          y bronco V8, especialmente cuando pulsamos el botón que abre las válvulas de escape para hacer el máximo "ruido" posible.
-          Con semejante sinfonía, es fácil perdonar que las cuatro salidas de escape sean falsas...
-        </p>
-      </div>
-    </div>
-    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify caja-texto">
-      <div class="col-lg-6">
-        <div class="card text-center w-100 "><!-- class:float-left -->
-            <img class="card-img-top" src="../img/analisis/c43_interior.jpg" alt="Instrumentacion digital">
-            <div class="card-body">
-              <p class="card-text">La instrumentación digital es abundante.</p>
-            </div>
-          </div>
-      </div>
-      <div class="col-lg-6 contenedor-dcha">
-        <p>
-          Con el modo de conducción Sport+ seleccionado, también es inevitable apagar el excepcional equipo de sonido Burmester para
-          deleitarse con los <b>petardeos</b> que se producen en cada cambio de marcha. La transmisión automática AMG Speedshift TCT
-          9G de nueve velocidades ha sido optimizada con una gestión electrónica que logra transiciones más rápidas entre marchas.
-          También es posible manejar manualmente la caja de cambios a través de las levas situadas detrás del volante, aunque, con el
-          pie a fondo, lo ideal es centrarse en la carretera y dejar que la caja automática haga su trabajo, porque el V6 biturbo
-          empuja con unos contundentes 520 Nm desde apenas 2.500 rpm. Los 390 CV hacen que nos olvidemos de que estamos al volante
-          de una versión de acceso a la gama, mientras que la tracción total 4Matic, ayudada por los neumáticos ContiSport Contact
-          6, garantiza la máxima motricidad en cada curva.
-        </p>
-      </div>
-    </div>
-    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify separar-arriba">
-      <div class="col-lg-12">
-        <p>
-          A la hora de circular de forma más tranquila, el Drive Select, mediante el programa Comfort, cambia el carácter del
-          conjunto de manera radical. Con este modo de conducción seleccionado y las válvulas de escape en su posición menos
-          ruidosa, el coupé alemán es resulta tan cómodo y confortable como se espera de un Mercedes. La suspensión variable es
-          menos firme que en los modos Sport y Sport+, la respuesta del acelerador es menos reactiva y la dirección pierde dureza.
-        </p>
-      </div>
-    </div>
-    <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
-      <div class="col-lg-12">
-        <h2>Conclusión</h2>
-      </div>
-      <div class="col-lg-12">
-        <p>
-          Los Mercedes-AMG C43 4MATIC poseen un volante específico AMG y una suspensión adaptativa, integrada plenamente en el
-          control de chasis adaptativo – llámalo selector de modos de conducción – AMG DRIVE SELECT. Es un buen esfuerzo por parte
-          de Mercedes-AMG, tratando de conferir un carácter <b>más deportivo y agresivo</b> a esta berlina que, por nuestra parte,
-          nos ha encantado.
-        </p>
-      </div>
-    </div>
-    <hr class="my-4 w-100">
-    <!-- INICIO GALERIA -->
-    <div class="gallery">
-      <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
-        <div class="col-lg-4">
-          <figure>
-            <img src="../img/analisis/gallery/c43-1.jpg" alt="" />
-            <!--<figcaption>Daytona Beach <small>United States</small></figcaption>-->
-          </figure>
-        </div>
-        <div class="col-lg-4">
-          <figure>
-            <img src="../img/analisis/gallery/c43-2.jpg" alt="" />
-            <!--<figcaption>Russia, gorod Severomorsk <small>Russia</small></figcaption>-->
-          </figure>
-        </div>
-        <div class="col-lg-4">
-          <figure>
-            <img src="../img/analisis/gallery/c43-3.jpg" alt="" />
-            <!--<figcaption>Bad Pyrmont <small>Deutschland</small></figcaption>-->
-          </figure>
-        </div>
-      </div>
-      <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
-        <div class="col-lg-4">
-          <figure>
-            <img src="../img/analisis/gallery/c43-4.jpg" alt="" />
-            <!--<figcaption>Yellowstone National Park <small>United States</small></figcaption>-->
-          </figure>
-        </div>
-        <div class="col-lg-4">
-          <figure>
-            <img src="../img/analisis/gallery/c43-5.jpg" alt="" />
-            <!--<figcaption>Quiraing, Portree <small>United Kingdom</small></figcaption>-->
-          </figure>
-        </div>
-        <div class="col-lg-4">
-          <figure>
-            <img src="../img/analisis/gallery/c43-6.jpg" alt="" />
-            <!--<figcaption>Highlands <small>United States</small></figcaption>-->
-          </figure>
-        </div>
-      </div>
-
-      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="display:none;">
-        <symbol id="close" viewBox="0 0 18 18">
-          <path fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M9,0.493C4.302,0.493,0.493,4.302,0.493,9S4.302,17.507,9,17.507
-            S17.507,13.698,17.507,9S13.698,0.493,9,0.493z M12.491,11.491c0.292,0.296,0.292,0.773,0,1.068c-0.293,0.295-0.767,0.295-1.059,0
-            l-2.435-2.457L6.564,12.56c-0.292,0.295-0.766,0.295-1.058,0c-0.292-0.295-0.292-0.772,0-1.068L7.94,9.035L5.435,6.507
-            c-0.292-0.295-0.292-0.773,0-1.068c0.293-0.295,0.766-0.295,1.059,0l2.504,2.528l2.505-2.528c0.292-0.295,0.767-0.295,1.059,0
-            s0.292,0.773,0,1.068l-2.505,2.528L12.491,11.491z"/>
-        </symbol>
-      </svg>
-      
-      
-      
-    </div>
-    <!-- FIN GALERIA -->
-
+<!--Incluyo el autor del contenido-->
+<?php
+//Conexion y consulta
+$con = new Conexion();
+$con->set_charset("utf8"); //Establecemos la codificacion adecuada
+$sql = "SELECT *
+        FROM usuario,contenido
+        WHERE contenido.id_usuario_autor=usuario.id";
+$resultado = $con->query($sql);
+if($con->affected_rows){ //Devuelve 0 o un numero
+  while($fila = $resultado->fetch_object()){
+      $u = new Usuario($fila->id, $fila->email, $fila->pass, $fila->nombre, $fila->apellido1, $fila->apellido2, $fila->fecha_nac, $fila->pais, $fila->cod_postal, $fila->telefono, $fila->rol); //Nombre de las columnas de la tabla
+    }
+  $con->close();
+}
+?>
+<div class="container">
+  <section id="scroll" class="projects-section bg-light reducirMargenSuperior">
     <div class="row justify-content-center no-gutters mb-5 mb-lg-0 text-justify">
       <hr class="my-4 w-100">
       <div class="col-lg-12">
-        <h3>Relacionado</h3>
-      </div>
-      <div class="col-lg-12">
-        <ul>
-          <li>
-            <a href="../noticias/noticia-mercedes.php">Mercedes-Benz Clase B, desvelado en París</a>
-          </li>
-          <li>
-            <a href="../analisis/">Más análisis...</a>
-          </li>
-        </ul>
-      </div>
-      <hr class="my-4 w-100">
-      <div class="col-lg-12">
-        <h3>Fuente(s)</h3>
-      </div>
-      <div class="col-lg-12">
-        <ul>
-          <li>
-            <a href="https://www.diariomotor.com/noticia/mercedes-amg-c-43-4matic-2018/">Enlace 1</a>
-          </li>
-          <li>
-            <a href="https://www.autofacil.es/mercedes-benz/clase-c/2018/08/14/prueba-mercedes-amg-c-43/45850.html">Enlace 2</a>
-          </li>
-        </ul>
+        <h5>Autor: <?php echo $u->nombre ?></h5>
       </div>
     </div>
   </section>
-  </div>
+</div>
+
+
+<!--Resto de scripts-->
+<?php include("../includes/navigation.php"); ?>
 
 <?php include("../includes/gallery-script.php");?>
 
@@ -238,3 +89,4 @@
 
 </body>
 </html>
+

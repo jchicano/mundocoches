deedaa198d50d1c4f9ec9716f1a44313f01ad844
davidruiz120 <davidruiz.99@hotmail.com>
2019-01-25 09:11:27 +0100

    Menu Sign In funcional

--- includes/head-tag-contents.php
+++ includes/head-tag-contents.php
@@ -77,3 +77,6 @@
 
 <!-- Estilos CSS propios || Icono de usuario -->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">

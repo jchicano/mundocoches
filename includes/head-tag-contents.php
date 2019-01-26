35caf5648ff342afcfbe61e1740e0b19ebe12a20
Jesús David Chicano Galindo <JesusD@MacBook-Pro-de-Jesus.local>
2019-01-26 16:54:44 +0100

    Editado comentario

--- includes/head-tag-contents.php
+++ includes/head-tag-contents.php
@@ -75,7 +75,7 @@
 	}
 ?>
 
-<!-- Estilos CSS propios || Icono de usuario -->
+<!-- Icono de usuario del menu-->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>

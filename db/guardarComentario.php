4bbc267018f320db7ba712871f3f844f98e5e8ba
Jesús David Chicano Galindo <JesusD@pc179-10.14004580.14.andared.ced.junta-andalucia.es>
2019-01-29 14:24:54 +0100

    Creado script en el que se guarda el comentario del usuario

---
+++ db/guardarComentario.php
@@ -0,0 +1,52 @@
+<?php
+require_once("Conexion.php");
+require_once("Usuario.php");
+require_once("Contenido.php");
+
+//Guardamos el id de usuario logueado
+$idUsuario = isset($_GET['id'])?$_GET['id']:$_POST['id'];
+$idUsuario = 1; //PRODUCCION esta puesto id usuario fijo
+
+//Guardamos la URL de la pagina anterior, para saber a que analisis nos referimos
+$urlPaginaAnteriorIncorrecta = $_SERVER['HTTP_REFERER'];
+$stringInicial = "http://localhost:8080"; //String que queremos eliminar de la URL //PRODUCCION cambiar esta URL por la del dominio
+if(substr($urlPaginaAnteriorIncorrecta, 0, strlen($stringInicial)) === $stringInicial) //Calculamos la longitud de ese string y lo restamos
+    $urlPaginaAnteriorCorrecta = substr($urlPaginaAnteriorIncorrecta, strlen($stringInicial));
+
+//Guardamos la nota que ha puesto el usuario
+$comentario = isset($_GET['comentario'])?$_GET['comentario']:$_POST['comentario'];
+
+//Creamos la conexión a la BD
+$con = new Conexion();
+$con->set_charset("utf8"); //Establecemos la codificacion adecuada
+$sql = "SELECT * FROM contenido where url='$urlPaginaAnteriorCorrecta'"; //Selecciono el id del articulo actual
+$resultado = $con->query($sql);
+if($con->affected_rows){ //Devuelve 0 o un numero
+    $resultado = $con->query($sql);
+    $fila = $resultado->fetch_object();
+    $c = new Contenido($fila->id, $fila->url, $fila->titulo, $fila->fecha_publicacion, $fila->id_usuario_autor, $fila->texto); //Nombre de las columnas de la tabla
+    $idArticuloActual = $c->id;
+}
+$sql = "UPDATE valoracion
+        SET comentario = '$comentario'
+        WHERE id_usuario = $idUsuario && id_contenido = $idArticuloActual";
+$resultado = $con->query($sql);
+
+if($con->affected_rows && $con->errno == 0){
+    echo 1;
+    //echo '{"correcto": "1"}';
+}
+else { //Entraría aquí si no existe ninguna valoracion con el usuario actual en el articulo actual
+    /*$sql = "UPDATE valoracion
+    SET nota = $nota
+    WHERE id_usuario=$idUsuario && id_contenido=$idArticuloActual";
+    $resultado = $con->query($sql);*/
+    /*if($con->affected_rows && $con->errno == 0){ //Devuelve 0 o un numero
+        echo 1;
+    }
+    else{*/
+        //echo '{"correcto": "0"}';
+        echo 0;
+    //}
+}
+$con->close();

fb119a5bc621de47135b79a7f2aed9de43bb9885
davidruiz120 <davidruiz.99@hotmail.com>
2019-01-30 17:00:58 +0100

    Página registro (funcional) v0.5

---
+++ db/registrarUsuario.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once("db/Conexion.php");
+
+// Conexión a la BBDD
+$con = new Conexion();
+$con->set_charset("utf8"); //Establecemos la codificacion adecuada
+
+if(isset($_REQUEST["registrarse"])){
+    // Guardamos los datos del registro en variables
+    $email = $_POST["inputEmail"];
+    $password = $_POST["inputPassword"];
+    $name = $_POST["inputName"];
+    $surname1 = $_POST["inputSurname1"];
+    $surname2 = $_POST["inputSurname2"];
+    $date = $_POST["inputDate"];
+    $country = $_POST["inputCountry"];
+    $zipcode = $_POST["inputZipcode"];
+    $mobile = $_POST["inputMobile"];
+
+    $sql = "INSERT INTO usuario (email, pass, nombre, apellido1, apellido2, fecha_nac, pais, cod_postal, telefono, rol)
+    VALUES ('$email', '$password', '$name', '$surname1', '$surname2', '$date', '$country', '$zipcode', '$mobile', 0)";
+    $con->query($sql);
+    
+    $con->close();
+}
+
+?>
--- registro.php
+++ registro.php
@@ -3,8 +3,20 @@
 <html>
 <head>
   <?php include("includes/head-tag-contents.php");?>
+  <?php include("db/registrarUsuario.php"); ?>
   <link rel="stylesheet" href="css/marcas/fichatecnica.css">
   <link rel="stylesheet" href="css/registro.css">
+
+  <script src='https://www.google.com/recaptcha/api.js?render=6LfW4o0UAAAAAGzY6YfZAeBbqoJ1jIl15VxzgyIx'></script>
+  <script>
+        grecaptcha.ready(function() {
+            grecaptcha.execute('6LfW4o0UAAAAAGzY6YfZAeBbqoJ1jIl15VxzgyIx', {action: 'action_name'})
+            .then(function(token) {
+            // Verifica el token en el servidor.
+            });
+        });
+  </script>
+
 </head>
 <body>
 
@@ -28,63 +40,58 @@
         </div>
         <div class="card-body">
             <h5 class="card-title text-center">Registrarse</h5>
-            <p style="font-size: 12px;">Los campos con el símbolo * son obligatorios</p>
+            <p style="font-size: 12px;">Los campos con el símbolo (*) son obligatorios</p>
             <form action="" method="post" class="form-signin">
-            <div class="form-label-group"> <!-- Usuario -->
-                <input type="text" id="inputUserame" class="form-control" placeholder="Username" required autofocus>
-                <label for="inputUserame">Nombre de usuario *</label>
+            <div class="form-label-group"> <!-- Email -->
+                <input type="email" name="inputEmail" id="inputEmail" class="form-control" placeholder="Email address" required>
+                <label for="inputEmail">Email *</label>
             </div>
 
             <div class="form-label-group"> <!-- Contraseña -->
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <input type="password" name="inputPassword" id="inputPassword" class="form-control" placeholder="Password" required>
                 <label for="inputPassword">Contraseña *</label>
             </div>
 
             <hr>
 
             <div class="form-label-group"> <!-- Nombre -->
-                <input type="text" id="inputName" class="form-control" placeholder="Name" required>
+                <input type="text" name="inputName" id="inputName" class="form-control" placeholder="Name" required>
                 <label for="inputName">Nombre *</label>
             </div>
 
             <div class="form-label-group"> <!-- Apellido 1 -->
-                <input type="text" id="inputSurname1" class="form-control" placeholder="Surname1" required>
+                <input type="text" name="inputSurname1" id="inputSurname1" class="form-control" placeholder="Surname1" required>
                 <label for="inputSurname1">Apellido 1 *</label>
             </div>
 
             <div class="form-label-group"> <!-- Apellido 2 -->
-                <input type="text" id="inputSurname2" class="form-control" placeholder="Surname2">
+                <input type="text" name="inputSurname2" id="inputSurname2" class="form-control" placeholder="Surname2">
                 <label for="inputSurname2">Apellido 2</label>
             </div>
 
-            <div class="form-label-group"> <!-- Email -->
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required>
-                <label for="inputEmail">Email *</label>
-            </div>
-
             <div class="form-label-group"> <!-- Fecha de nacimiento -->
-                <input type="date" id="inputDate" class="form-control" placeholder="Date" required>
+                <input type="date" name="inputDate" id="inputDate" class="form-control" placeholder="Date" required>
                 <label for="inputDate">Fecha de nacimiento *</label>
             </div>
             
             <div class="form-label-group"> <!-- País -->
-                <input type="text" id="inputCountry" class="form-control" placeholder="Country" required>
+                <input type="text" name="inputCountry" id="inputCountry" class="form-control" placeholder="Country" required>
                 <label for="inputCountry">País *</label>
             </div>
 
             <div class="form-label-group"> <!-- Código postal -->
-                <input type="text" id="inputZipcode" class="form-control" placeholder="Zipcode" required>
+                <input type="text" name="inputZipcode" id="inputZipcode" class="form-control" placeholder="Zipcode" required>
                 <label for="inputZipcode">Código postal *</label>
             </div>
 
             <div class="form-label-group"> <!-- Teléfono -->
-                <input type="text" id="inputMobile" class="form-control" placeholder="Mobile" required>
+                <input type="text" name="inputMobile" id="inputMobile" class="form-control" placeholder="Mobile" required>
                 <label for="inputMobile">Teléfono *</label>
             </div>
 
             <hr>
 
-            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Registrarse</button>
+            <button class="btn btn-lg btn-primary btn-block text-uppercase" name="registrarse" type="submit">Registrarse</button>
 
             <hr class="my-4">
             <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit" disabled><i class="fab fa-google mr-2"></i> Iniciar sesión con Google</button>

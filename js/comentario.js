3d34d04999b202b6363be423ef5c79eaee2f88b6
Jesús David Chicano Galindo <JesusD@pc179-10.14004580.14.andared.ced.junta-andalucia.es>
2019-01-30 11:19:25 +0100

    Eliminadas funciones no usadas y añadido enlace para mostrar comentarios

--- js/comentario.js
+++ js/comentario.js
@@ -4,15 +4,15 @@
         //Modificamos la barra de tareas
         var toolbarOptions = [
             ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-            [{ 'align': [] }],
-            ['blockquote', /*'code-block'*/],
+            //[{ 'align': [] }],
+            //['blockquote', /*'code-block'*/],
 
             //[{ 'header': 1 }, { 'header': 2 }],               // custom button values
             ['clean'],                                         // remove formatting button
-            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            [/*{ 'list': 'ordered'},*/ { 'list': 'bullet' }],
             //[{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-            [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-            //[{ 'direction': 'rtl' }],                         // text direction
+            //[{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+            [{ 'direction': 'rtl' }],                         // text direction
 
             //[{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
             //[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
@@ -34,67 +34,51 @@
             return tempCont.getElementsByClassName("ql-editor")[0].innerHTML;
         }
 
-        function mostrarCuadro(datos){
-            alert(datos);
-        }
-
-        function procesarDatosComentario(datosRespuesta){ //MEOLLO
-            console.log("datos: "+datosRespuesta);
-            if(datosRespuesta == 0){ //Si no ha habido inserción en la BD
-                console.log("mal");
-                msg = "Primero debes valorar el artículo."
-                responseMessage(msg)
-            }
-            else{
-                console.log("bien");
-                msg = "Comentario agregado correctamente."
-                responseMessage(msg)
-            }
-            //alert();
-        }
 
 
         //Funcion para guardar el comentario introducido por el usuario
         $('#btnEnviar').on("click", function(){
         
-        var deltaContent = quill.getContents();
-        var htmlText = quillGetHTML(deltaContent);
-        
-        var datos={comentario : htmlText};
-        //$.getJSON('../db/guardarComentario.php',datos,mostrarCuadro);
-        
-        $.ajax({
-            // En data puedes utilizar un objeto JSON, un array o un query string
-            data: datos,
-            //Cambiar a type: POST si necesario
-            type: "GET",
-            // Formato de datos que se espera en la respuesta
-            dataType: "json",
-            // URL a la que se enviará la solicitud Ajax
-            url: "../db/guardarComentario.php",
-        })
-         .done(function(data) {
-             console.log(data.correcto);
-            if(data.correcto == "insertado"){
-                $("#modalComentarioMensaje").text("Comentario añadido correctamente.");
+            var deltaContent = quill.getContents();
+            var htmlText = quillGetHTML(deltaContent);
+            
+            var datos={comentario : htmlText};
+            //$.getJSON('../db/guardarComentario.php',datos,mostrarCuadro);
+            
+            $.ajax({
+                // En data puedes utilizar un objeto JSON, un array o un query string
+                data: datos,
+                //Cambiar a type: POST si necesario
+                type: "GET",
+                // Formato de datos que se espera en la respuesta
+                dataType: "json",
+                // URL a la que se enviará la solicitud Ajax
+                url: "../db/guardarComentario.php",
+            })
+            .done(function(data) {
+                console.log(data.correcto);
+                if(data.correcto == "insertado"){
+                    $("#modalComentarioMensaje").text("Comentario añadido correctamente.");
+                    $("#modalComentario").modal();
+                }
+                else if(data.correcto == "comentarioRepetido"){
+                    $("#modalComentarioMensaje").text("El comentario ya existe.");
+                    $("#modalComentario").modal();
+                }
+                else{
+                    $("#modalComentarioMensaje").text("Introduce primero una valoración.");
+                    $("#modalComentario").modal();
+                }
+            })
+            .fail(function( jqXHR, textStatus, errorThrown ) {
+                $("#modalComentarioMensaje").text("Error al procesar la petición ("+textStatus+" - "+errorThrown+")");
                 $("#modalComentario").modal();
-            }
-            else if(data.correcto == "comentarioRepetido"){
-                $("#modalComentarioMensaje").text("El comentario ya existe.");
-                $("#modalComentario").modal();
-            }
-            else{
-                $("#modalComentarioMensaje").text("Introduce primero una valoración.");
-                $("#modalComentario").modal();
-            }
-         })
-         .fail(function( jqXHR, textStatus, errorThrown ) {
-            $("#modalComentarioMensaje").text("Error al procesar la petición ("+textStatus+" - "+errorThrown+")");
-            $("#modalComentario").modal();
+            });
+
         });
 
-        //
-        //mostrarCuadro();
+        $("#enlaceMostrar").click(function(){
+            $("#enlaceMostrar").hide();
         });
     });
 })(jQuery);

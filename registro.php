f0056d84e476434104a0f0d0e78986b6a358a476
daviddar13 <pese1313@gmail.com>
2019-01-31 12:56:04 +0100

    captcha

--- registro.php
+++ registro.php
@@ -6,6 +6,7 @@
   <?php include("db/registrarUsuario.php"); ?>
   <link rel="stylesheet" href="css/marcas/fichatecnica.css">
   <link rel="stylesheet" href="css/registro.css">
+  <link rel="stylesheet" href="css/captcha.css">
 
   <script src='https://www.google.com/recaptcha/api.js?render=6LfW4o0UAAAAAGzY6YfZAeBbqoJ1jIl15VxzgyIx'></script>
   <script>
@@ -41,7 +42,7 @@
         <div class="card-body">
             <h5 class="card-title text-center">Registrarse</h5>
             <p style="font-size: 12px;">Los campos con el símbolo (*) son obligatorios</p>
-            <form action="" method="post" class="form-signin">
+            <form action="" method="post" class="form-signin"  onsubmit="return checkform(this);">
             <div class="form-label-group"> <!-- Email -->
                 <input type="email" name="inputEmail" id="inputEmail" class="form-control" placeholder="Email address" required>
                 <label for="inputEmail">Email *</label>
@@ -91,12 +92,88 @@
 
             <hr>
 
-            <button class="btn btn-lg btn-primary btn-block text-uppercase" name="registrarse" type="submit">Registrarse</button>
+            <button class="btn btn-lg btn-primary btn-block text-uppercase" name="registrarse" type="submit" >Registrarse</button>
 
             <hr class="my-4">
             <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit" disabled><i class="fab fa-google mr-2"></i> Iniciar sesión con Google</button>
             
+            <!-- INICIO CAPTCHA -->
+            <hr>
+
+            <div class="capbox">
+
+                <div id="CaptchaDiv"></div>
+
+                <div class="capbox-inner">
+                    Escriba el número de arriba:<br>
+                    <input type="hidden" id="txtCaptcha">
+                    <input type="text" name="CaptchaInput" id="CaptchaInput" size="20"><br>
+
+                </div>
+            </div>
+
+            <br><br>
+            <!-- FIN CAPTCHA -->
+
             </form>
+
+            <script type="text/javascript">
+
+                // Script para el captcha
+
+                function checkform(theform) {
+
+                    var why = "";
+
+                    if(theform.CaptchaInput.value == "") {
+                        why += "Por favor introduce el código CAPTCHA.\n";
+                    }
+
+                    if(theform.CaptchaInput.value != "") {
+
+                        if(ValidCaptcha(theform.CaptchaInput.value) == false) {
+                            why += "El código CAPTCHA no coincide.\n";
+                        }
+                    }
+
+                    if(why != "") {
+                    alert(why);
+                    return false;
+                    }
+                }
+
+                var a = Math.ceil(Math.random() * 9)+ '';
+                var b = Math.ceil(Math.random() * 9)+ '';
+                var c = Math.ceil(Math.random() * 9)+ '';
+                var d = Math.ceil(Math.random() * 9)+ '';
+                var e = Math.ceil(Math.random() * 9)+ '';
+
+                var code = a + b + c + d + e;
+
+                document.getElementById("txtCaptcha").value = code;
+                document.getElementById("CaptchaDiv").innerHTML = code;
+
+                // Valida la entrada con el número generado
+                function ValidCaptcha() {
+
+                    var str1 = removeSpaces(document.getElementById('txtCaptcha').value);
+                    var str2 = removeSpaces(document.getElementById('CaptchaInput').value);
+
+                    if (str1 == str2) {
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+
+                // Elimina los espacios del código introducido y generado
+                function removeSpaces(string) {
+
+                    return string.split(' ').join('');
+                }
+
+            </script>
+
         </div>
         </div>
             

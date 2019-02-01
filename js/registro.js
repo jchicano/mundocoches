6fa1e8226a8051df35c081f744caa6d74b7001d5
davidruiz120 <davidruiz.99@hotmail.com>
2019-02-01 10:05:10 +0100

    Progresos en validar la contraseña (v.8)

---
+++ js/registro.js
@@ -0,0 +1,27 @@
+(function ($) {
+
+    $(document).ready(function(){
+        document
+            .getElementById('inputPassword')
+            .addEventListener('input', function(evt) {
+                const campo = evt.target,
+                    valido = document.getElementById('campoOK'),
+                    
+                    regex = /^(?=.*[a-záéíóúüñ]).*[A-ZÁÉÍÓÚÜÑ].*[$%&€]/;
+
+                //Se muestra un texto válido/inválido a modo de ejemplo
+                if (regex.test(campo.value)) {
+                    $('campoOK').addClass('errorPass');
+                    valido.innerText = "Contraseña válida";
+                    console.log("Estoy en el if");
+                } else {
+                    valido.innerText = "Contraseña no válida";
+                    console.log("Estoy en el else");
+                }
+            });
+    });
+
+
+})(jQuery); 
+
+
